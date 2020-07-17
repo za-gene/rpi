74d6e86e112559539161710104ab86654d792078
Mark Carter <alt.mcarter@gmail.com>
2020-07-17 14:25:06 +0100

    modified:   settime.py
    Should now be able to set time of device within 1 sec
    
    GMT is used. Arduino must correct for local time

--- dev09/settime.py
+++ dev09/settime.py
@@ -1,10 +1,30 @@
+import calendar
+import datetime
 import time
 import serial
-ser = serial.Serial(port="/dev/ttyUSB0", baudrate = 9600)  # open first serial port
-print(ser.portstr)       # check which port was really used
-time.sleep(7)
-data = b"h"
-ser.write(data)      # write a string
-#ser.write(0xa4)         # write a byte
-time.sleep(5)
-ser.close()             # close port
+
+def send(data):
+    ser = serial.Serial(port="/dev/ttyUSB0", baudrate = 9600) 
+    print(ser.portstr)       # check which port was really used
+    time.sleep(4)
+    bin = data.encode('utf-8')
+    #print(bin)
+    ser.write(bin)
+    time.sleep(5)
+    ser.close()
+
+def mk_cmd():
+    fudge = 5 # to account for delays in uploading
+    secs = time.time() + fudge
+    ts = datetime.datetime.fromtimestamp(secs)
+    gmt = time.gmtime(secs)
+    cmd = "SET " + time.strftime("%b %d %Y %H:%M:%S", gmt) +"\r"
+    print(cmd)
+    return cmd
+
+def main():
+    cmd = mk_cmd()
+    send(cmd)
+
+if __name__ == "__main__":
+    main()
