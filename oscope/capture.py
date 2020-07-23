1eec996e02618c199899535b99e3e0e258c40cec
Mark Carter <alt.mcarter@gmail.com>
2020-07-23 19:33:22 +0100

    impoved oscope capture and oner

--- oscope/capture.py
+++ oscope/capture.py
@@ -3,11 +3,28 @@ import timeit
 from time import sleep
 import time
 import spidev
+import argparse
+parser = argparse.ArgumentParser(description='Capture oscilloscope')
+parser.add_argument("-r", 
+        nargs="?", #dest='spi_rate', 
+        #const='spi_rate', action='store_const', i
+        #default = 1E6, 
+        type=int,
+        help='SPI rate (Hz) (default = 1,000,000)')
+args = parser.parse_args()
+if args.r == None:
+    spi_rate = 1000000
+else:
+    spi_rate = args.r
+#print(spi_rate)
+#exit(0)
+
 spi = spidev.SpiDev()
 #spi.open(0, CHIP_SELECT_0_OR_1)
 spi.open(0, 0)
-spi.max_speed_hz = 1000000
-spi.max_speed_hz =  500000
+#spi.max_speed_hz = 1000000
+#pspi.max_speed_hz =  500000
+spi.max_speed_hz =  spi_rate
 
 def fetch():
     #to_send = [0x01]
