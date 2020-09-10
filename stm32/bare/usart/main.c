e541270b1c923505f2f88c9e78158e89dc706e62
Mark Carter <alt.mcarter@gmail.com>
2020-09-10 13:41:25 +0100

    Improved usart for stm32.
    Compiles, but bin file is 385M for some reason

--- stm32/bare/usart/main.c
+++ stm32/bare/usart/main.c
@@ -1,120 +1,116 @@
 // Standard library includes.
 #include <stdint.h>
+/*
 #include <stdio.h>
 #include <string.h>
+*/
 
-// Device header file.
-#ifdef VVC_F1
-  #include "stm32f1xx.h"
-#elif VVC_L4
-  #include "stm32l4xx.h"
-#endif
+uint32_t SystemCoreClock = 8000000;
+//extern uint32_t _sidata, _sdata, _edata, _sbss, _ebss;
 
-uint32_t SystemCoreClock = 0;
-extern uint32_t _sidata, _sdata, _edata, _sbss, _ebss;
+#define VVC_F1
 
-// Reset handler: set the stack pointer and branch to main().
-__attribute__( ( naked ) ) void reset_handler( void ) {
-  // Set the stack pointer to the 'end of stack' value.
-  __asm__( "LDR r0, =_estack\n\t"
-           "MOV sp, r0" );
-  // Branch to main().
-  __asm__( "B main" );
-}
+// cribbed from blink sketch
+#define RCC_BASE      	0x40021000
+// reference page 51
+#define GPIOA_BASE    	0x40010800
+#define USART2_BASE	0x40004400
+
+#define RCC_APB1ENR   *(volatile uint32_t *)(RCC_BASE   + 0x1C) // page 148
+#define RCC_APB1ENR_USART2EN	(1<<17)
+#define RCC_APB2ENR   *(volatile uint32_t *)(RCC_BASE   + 0x18)
+#define RCC_APB2ENR_IOPAEN	(1<<2)
+#define GPIOA_CRL     *(volatile uint32_t *)(GPIOA_BASE + 0x00)
+
+#define GPIO_CRL_CNF2_Pos 10 // page 171
+#define GPIO_CRL_CNF3_Pos 14
+#define GPIO_CRL_MODE2_Pos 8
+#define GPIO_CRL_MODE3_Pos 12
+#define GPIO_CRL_CNF2 (0x3<<GPIO_CRL_CNF2_Pos)
+#define GPIO_CRL_CNF3 (0x3<<GPIO_CRL_CNF3_Pos)
+#define GPIO_CRL_MODE2 (0x3<<GPIO_CRL_MODE2_Pos)
+#define GPIO_CRL_MODE3 (0x3<<GPIO_CRL_MODE3_Pos)
+#define USART_BRR_DIV_Fraction_Pos 0 // page 820
+#define USART_BRR_DIV_Mantissa_Pos 4
+#define USART_CR1_RE (1<<2) // page 821
+#define USART_CR1_TE (1<<3)
+#define USART_CR1_UE (1<<13)
+#define USART_SR_RXNE (1 << 5) // page 818
+#define USART_SR_TXE (1 << 7)
+
+// page 827
+#define USART_SR 0x00
+#define USART_DR 0x04
+#define USART_BRR 0x08
+#define USART_CR1 0x0C
+#define USART_CR2 0x10
+#define USART_CR3 0x14
+#define USART_GTPR 0x18
+
+#define USART2_SR *(volatile uint32_t *)(USART2_BASE+USART_SR)
+#define USART2_DR *(volatile uint32_t *)(USART2_BASE+USART_DR)
+#define USART2_BRR *(volatile uint32_t *)(USART2_BASE+USART_BRR)
+#define USART2_CR1 *(volatile uint32_t *)(USART2_BASE+USART_CR1)
 
 /**
  * Main program.
  */
 int main( void ) {
   // Copy initialized data from .sidata (Flash) to .data (RAM)
-  memcpy( &_sdata, &_sidata, ( ( void* )&_edata - ( void* )&_sdata ) );
+  //memcpy( &_sdata, &_sidata, ( ( void* )&_edata - ( void* )&_sdata ) );
   // Clear the .bss section in RAM.
-  memset( &_sbss, 0x00, ( ( void* )&_ebss - ( void* )&_sbss ) );
+  //memset( &_sbss, 0x00, ( ( void* )&_ebss - ( void* )&_sbss ) );
 
   // Set the core system clock speed.
   #ifdef VVC_F1
     // Default clock source is the 8MHz internal oscillator.
     SystemCoreClock = 8000000;
   #elif VVC_L4
-    // Enable floating-point unit.
-    SCB->CPACR    |=  ( 0xF << 20 );
-
-    // Default clock source is the "multi-speed" internal oscillator.
-    // Switch to the 16MHz HSI oscillator.
-    RCC->CR |=  ( RCC_CR_HSION );
-    while ( !( RCC->CR & RCC_CR_HSIRDY ) ) {};
-    RCC->CFGR &= ~( RCC_CFGR_SW );
-    RCC->CFGR |=  ( RCC_CFGR_SW_HSI );
-    while ( ( RCC->CFGR & RCC_CFGR_SWS ) != RCC_CFGR_SWS_HSI ) {};
-    SystemCoreClock = 16000000;
   #endif
 
   // Enable peripheral clocks and set up GPIO pins.
   #ifdef VVC_F1
     // Enable peripheral clocks: GPIOA, USART2.
-    RCC->APB1ENR  |=  ( RCC_APB1ENR_USART2EN );
-    RCC->APB2ENR  |=  ( RCC_APB2ENR_IOPAEN );
+    RCC_APB1ENR  |=  ( RCC_APB1ENR_USART2EN );
+    RCC_APB2ENR  |=  ( RCC_APB2ENR_IOPAEN );
     // Configure pins A2, A3 for USART2.
-    GPIOA->CRL    &= ~( GPIO_CRL_MODE2 |
+    GPIOA_CRL    &= ~( GPIO_CRL_MODE2 |
                         GPIO_CRL_CNF2 |
                         GPIO_CRL_MODE3 |
                         GPIO_CRL_CNF3 );
-    GPIOA->CRL    |= ( ( 0x1 << GPIO_CRL_MODE2_Pos ) |
+    GPIOA_CRL    |= ( ( 0x1 << GPIO_CRL_MODE2_Pos ) |
                        ( 0x2 << GPIO_CRL_CNF2_Pos ) |
                        ( 0x0 << GPIO_CRL_MODE3_Pos ) |
                        ( 0x1 << GPIO_CRL_CNF3_Pos ) );
   #elif VVC_L4
-    // Enable peripheral clocks: GPIOA, USART2.
-    RCC->APB1ENR1 |= ( RCC_APB1ENR1_USART2EN );
-    RCC->AHB2ENR  |= ( RCC_AHB2ENR_GPIOAEN );
-    // Configure pins A2, A15 for USART2 (AF7).
-    GPIOA->MODER    &= ~( ( 0x3 << ( 2 * 2 ) ) |
-                          ( 0x3 << ( 15 * 2 ) ) );
-    GPIOA->MODER    |=  ( ( 0x2 << ( 2 * 2 ) ) |
-                          ( 0x2 << ( 15 * 2 ) ) );
-    GPIOA->OTYPER   &= ~( ( 0x1 << 2 ) |
-                          ( 0x1 << 15 ) );
-    GPIOA->OSPEEDR  &= ~( ( 0x3 << ( 2 * 2 ) ) |
-                          ( 0x3 << ( 15 * 2 ) ) );
-    GPIOA->OSPEEDR  |=  ( ( 0x2 << ( 2 * 2 ) ) |
-                          ( 0x2 << ( 15 * 2 ) ) );
-    GPIOA->AFR[ 0 ] &= ~( ( 0xF << ( 2 * 4 ) ) );
-    GPIOA->AFR[ 0 ] |=  ( ( 0x7 << ( 2 * 4 ) ) );
-    GPIOA->AFR[ 1 ] &= ~( ( 0xF << ( ( 15 - 8 ) * 4 ) ) );
-    GPIOA->AFR[ 1 ] |=  ( ( 0x3 << ( ( 15 - 8 ) * 4 ) ) );
   #endif
 
   // Set the baud rate to 9600.
   uint16_t uartdiv = SystemCoreClock / 9600;
   #ifdef VVC_F1
-    USART2->BRR = ( ( ( uartdiv / 16 ) << USART_BRR_DIV_Mantissa_Pos ) |
+    USART2_BRR = ( ( ( uartdiv / 16 ) << USART_BRR_DIV_Mantissa_Pos ) |
                     ( ( uartdiv % 16 ) << USART_BRR_DIV_Fraction_Pos ) );
   #elif VVC_L4
-    USART2->BRR = uartdiv;
   #endif
 
   // Enable the USART peripheral.
-  USART2->CR1 |= ( USART_CR1_RE | USART_CR1_TE | USART_CR1_UE );
+  USART2_CR1 |= ( USART_CR1_RE | USART_CR1_TE | USART_CR1_UE );
 
   // Main loop: wait for a new byte, then echo it back.
   char rxb = '\0';
   while ( 1 ) {
     // Receive a byte of data.
     #ifdef VVC_F1
-      while( !( USART2->SR & USART_SR_RXNE ) ) {};
-      rxb = USART2->DR;
+      while( !( USART2_SR & USART_SR_RXNE ) ) {};
+      rxb = USART2_DR;
     #elif VVC_L4
-      while( !( USART2->ISR & USART_ISR_RXNE ) ) {};
-      rxb = USART2->RDR;
     #endif
 
     // Re-transmit the byte of data once the peripheral is ready.
     #ifdef VVC_F1
-      while( !( USART2->SR & USART_SR_TXE ) ) {};
-      USART2->DR = rxb;
+      while( !( USART2_SR & USART_SR_TXE ) ) {};
+      USART2_DR = rxb;
     #elif VVC_L4
-      while( !( USART2->ISR & USART_ISR_TXE ) ) {};
-      USART2->TDR = rxb;
     #endif
   }
 }
