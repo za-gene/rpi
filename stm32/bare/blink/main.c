733cac991d325dffc4f3449a033718ec83e1c972
Mark Carter <alt.mcarter@gmail.com>
2020-09-11 12:36:38 +0100

    prettified stm32 blink sketch

--- stm32/bare/blink/main.c
+++ stm32/bare/blink/main.c
@@ -48,33 +48,33 @@ typedef struct
 
 void main(void)
 {
-    RCC_APB2ENR |= RCC_IOPCEN; // enable port C
-    GPIOC->CRH   &= 0xFF0FFFFF;
-    GPIOC->CRH   |= 0x00200000;
+	RCC_APB2ENR |= RCC_IOPCEN; // enable port C
+	GPIOC->CRH   &= 0xFF0FFFFF;
+	GPIOC->CRH   |= 0x00200000;
 
-    // Set PB13 into a high output state
-    RCC_APB2ENR |= RCC_IOPBEN; // enable port B
-    // set PB13 for output
-    GPIOB->CRH   &= 0xFF0FFFFF;
-    GPIOB->CRH   |= 0x00200000;
-    // set pin PB13 high
-    GPIOB->ODR |= GPIO13;
+	// Set PB13 into a high output state
+	RCC_APB2ENR |= RCC_IOPBEN; // enable port B
+	// set PB13 for output
+	GPIOB->CRH   &= 0xFF0FFFFF;
+	GPIOB->CRH   |= 0x00200000;
+	// set pin PB13 high
+	GPIOB->ODR |= GPIO13;
 
-    // set PB1 for output and set it high
-    GPIOB->CRL  &= 0xFFFFFF0F;
-    GPIOB->CRL  |= 0x00000020;
-    GPIOB->ODR  |= GPIO1;
+	// set PB1 for output and set it high
+	GPIOB->CRL  &= 0xFFFFFF0F;
+	GPIOB->CRL  |= 0x00000020;
+	GPIOB->ODR  |= GPIO1;
 
 
-    int i = 666;
-    while(1)
-    {
-        //GPIOC_ODR |=  GPIO13;
-	GPIOC->ODR |= GPIO13;
-        for (i = 0; i < 500000; i++); // arbitrary delay
-        GPIOC->ODR &= ~GPIO13;
-        for (i = 0; i < 500000; i++); // arbitrary delay
-    }
+	int i = 666;
+	while(1)
+	{
+		//GPIOC_ODR |=  GPIO13;
+		GPIOC->ODR |= GPIO13;
+		for (i = 0; i < 500000; i++); // arbitrary delay
+		GPIOC->ODR &= ~GPIO13;
+		for (i = 0; i < 500000; i++); // arbitrary delay
+	}
 }
 
 
