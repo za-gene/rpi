b5f547f3caf603b4f9f3d4f7161435de64560244
Mark Carter <alt.mcarter@gmail.com>
2020-09-11 20:31:42 +0100

    usart stuff
    data section now works

--- stm32/bare/usart/crt.s
+++ stm32/bare/usart/crt.s
@@ -6,5 +6,24 @@
 .word _reset
 .thumb_func
 _reset:
-    bl main
-    b .
+	b skip
+
+	// copy initialised data from .sidata (flash) to .data (ram)	
+	ldr r0, =sdata
+	//ldr r0, [r0]
+	ldr r1, =sidata
+	//ldr r1, [r1]
+	ldr r2, =edata
+	//ldr r2, [r2]
+	sub r2, r2, r0
+	bl memcpy_usr
+skip:
+	bl init_mem
+	bl main
+	b .
+
+.data 
+.align 4
+sdata: .word _sdata
+sidata: .word _sidata
+edata: .word _edata
--- stm32/bare/usart/main.c
+++ stm32/bare/usart/main.c
@@ -1,4 +1,8 @@
+//#include <string.h>
+#include <stddef.h> // for size_t
 #include "../blue.h"
+
+extern uint32_t _sidata, _sdata, _edata, _sbss, _ebss;
 
 // cribbed from blink sketch
 #define RCC_BASE      	0x40021000
@@ -54,11 +58,51 @@ void puts2(const char* s)
 	putc2('\n');
 }
 
-void main() {
+char greeting[] = "Hello from bare metal usart 8";
+
+void* memcpy_usr(void* dst, const void* src, size_t n) {
+	// Copies n bytes from src to dst
+
+	// Since we cannot dereference a void* ptr,
+	// we first typecast it to a char* ptr
+	// and then do the copying byte by byte,
+	// since a char* ptr references a single byte
+	char* char_dst = (char*) dst;
+	char* char_src = (char*) src;
+
+	for (int i=0; i<n; i++) {
+		*char_dst++ = *char_src++;
+	}
+
+	return dst;
+}
+
+void *memset_usr(void *b, int c, int len)
+{
+	int           i;
+	unsigned char *p = b;
+	i = 0;
+	while(len > 0)
+	{
+		*p = c;
+		p++;
+		len--;
+	}
+	return(b);
+}
+
+void init_mem()
+{
+	// not called explicitly, but done in crt.s prior to calling main()
+
 	// Copy initialized data from .sidata (Flash) to .data (RAM)
-	//memcpy( &_sdata, &_sidata, ( ( void* )&_edata - ( void* )&_sdata ) );
+	memcpy_usr( &_sdata, &_sidata, ( ( void* )&_edata - ( void* )&_sdata ) );
 	// Clear the .bss section in RAM.
-	//memset( &_sbss, 0x00, ( ( void* )&_ebss - ( void* )&_sbss ) );
+	memset_usr( &_sbss, 0x00, ( ( void* )&_ebss - ( void* )&_sbss ) );
+}
+
+void main() {
+
 
 	// Set the core system clock speed.
 	// Default clock source is the 8MHz internal oscillator.
@@ -90,7 +134,6 @@ void main() {
 	// Main loop: wait for a new byte, then echo it back.
 	char rxb = '\0';
 	putc2('\a'); // beep
-	char greeting[] = "Hello from bare metal usart";
 	puts2(greeting);
 	while ( 1 ) {
 		// Receive a byte of data.
