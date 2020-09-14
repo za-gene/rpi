83fb491f28c97b4658b1469232d4d69fc4939e1f
Mark Carter <alt.mcarter@gmail.com>
2020-09-14 16:31:37 +0100

    tinkering around with timer interrupts

--- stm32/bare/04-timer-interrupt/README.md
+++ stm32/bare/04-timer-interrupt/README.md
@@ -2,4 +2,5 @@
 
 Don't work
 
+Startup file : https://github.com/getoffmyhack/STM32F103-Bare-Metal/blob/master/CMSIS/Device/STM32F1xx/src/startup_stm32f103xb.s
 
--- stm32/bare/04-timer-interrupt/main.c
+++ stm32/bare/04-timer-interrupt/main.c
@@ -18,18 +18,22 @@
 #define TIM_EGR_TG (1<<6)
 
 //void __attribute__ ((interrupt ("IRQ"))) myhandler()
-void myhandler()
+void TIM4_IRQHandler()
 {
-	//puts("hi");
+	puts("hi");
+	//TIM4->EGR |= TIM_EGR_UG; // send an update even to reset timer and apply settings
+	//TIM4->SR &= ~0x01; // clear UIF
+	TIM4->DIER |= 0x01; // UIE
 }
 
 void setup_timer()
 {
 	RCC_APB1ENR |= RCC_APB1ENR_TIM4EN;
 	TIM4->PSC=7999;
-	TIM4->ARR=000;
+	TIM4->ARR=1000;
 	TIM4->EGR |= TIM_EGR_UG; // send an update even to reset timer and apply settings
-	//TIM4->EGR |= (TIM_EGR_TG | TIM_EGR_UG);
+	TIM4->EGR |= (TIM_EGR_TG | TIM_EGR_UG);
+	TIM4->DIER |= 0x01; // UIE enable interrupt
 	TIM4->CR1 |= TIM_CR1_CEN;
 	puts("Timer setup");
 }
@@ -37,24 +41,38 @@ void setup_timer()
 void main() 
 {
 	init_serial();
-	puts("04-timer-interrupt started");
+	puts("04-timer-interrupt started 4");
 	char msg[40];
 
 	setup_timer();
 
-	NVIC_TIM4 = (uint32_t) myhandler; // seems to cause a problem
-	TIM4->DIER |= TIM_DIER_UIE;
+	//NVIC_TIM4 = (uint32_t) myhandler; // seems to cause a problem
+	//TIM4->DIER |= (TIM_DIER_UIE | TIM_DIER_TIE);
 	puts("Interrupt set");
 
 
+	asm("CPSIE I"); // enable interrupts
 
 	putchar('\a'); // beep
-	/*
+
 	int secs = 0;
 	while(1) {
 		itoa(secs++, msg, 10);
 		puts(msg);
-		delay(1000);
+		for(int i=0; i< 1000000; i++);
+		//delay(1000);
 	}
-	*/
+
+	//while(1);
+}
+
+void SystemInit()
+{
+	// TODO
+}
+
+void __libc_init_array()
+{
+	// TODO
+	
 }
