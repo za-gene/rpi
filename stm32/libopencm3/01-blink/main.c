76a3cf368995217a86c9dbeb760702999baab213
Mark Carter <alt.mcarter@gmail.com>
2020-09-23 17:45:15 +0100

    improved cm3 blink sketch

--- stm32/libopencm3/01-blink/main.c
+++ stm32/libopencm3/01-blink/main.c
@@ -1,26 +1,20 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+#define PORT_LED1 GPIOC
+#define PIN_LED1 13
+
+//#define LITTLE_BIT 50000UL
 int main(void) {
-        rcc_periph_clock_enable(RCC_LED1);
-#if defined(STM32F1) /* F1 is a precious snowflake */
-	gpio_set_mode(PORT_LED1, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, PIN_LED1);
-#else /* everyone else is sane */
-        gpio_mode_setup(PORT_LED1, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED1);
-#endif
+	//rcc_periph_clock_enable(RCC_LED1);
+	gpio_set_mode(PORT_LED1, GPIO_MODE_OUTPUT_2_MHZ, 
+			GPIO_CNF_OUTPUT_PUSHPULL, PIN_LED1);
 	gpio_set(PORT_LED1, PIN_LED1);
-#if defined(RCC_LED2)
-        rcc_periph_clock_enable(RCC_LED2);
-        gpio_mode_setup(PORT_LED2, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED2);
-#endif
 	while(1) {
 		/* wait a little bit */
-		for (int i = 0; i < LITTLE_BIT; i++) {
+		for (int i = 0; i < 500000UL; i++) {
 			__asm__("nop");
 		}
 		gpio_toggle(PORT_LED1, PIN_LED1);
-#if defined(RCC_LED2)
-		gpio_toggle(PORT_LED2, PIN_LED2);
-#endif
 	}
 }
