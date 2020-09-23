5a22ab00b7454539de0f1337558a93ba41cf3eae
Mark Carter <alt.mcarter@gmail.com>
2020-09-23 18:07:43 +0100

    main.c blink change

--- stm32/libopencm3/01-blink/main.c
+++ stm32/libopencm3/01-blink/main.c
@@ -6,13 +6,13 @@
 
 //#define LITTLE_BIT 50000UL
 int main(void) {
-	//rcc_periph_clock_enable(RCC_LED1);
+	rcc_periph_clock_enable(RCC_GPIOC);
 	gpio_set_mode(PORT_LED1, GPIO_MODE_OUTPUT_2_MHZ, 
 			GPIO_CNF_OUTPUT_PUSHPULL, PIN_LED1);
 	gpio_set(PORT_LED1, PIN_LED1);
 	while(1) {
 		/* wait a little bit */
-		for (int i = 0; i < 500000UL; i++) {
+		for (int i = 0; i < 800000UL; i++) {
 			__asm__("nop");
 		}
 		gpio_toggle(PORT_LED1, PIN_LED1);
