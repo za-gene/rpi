66281ebdd011e65cc2facbaedcced887561a69ec
Mark Carter <alt.mcarter@gmail.com>
2020-09-30 15:51:17 +0100

    Generalised gpio

--- stm32/bare/01-blink/main.c
+++ stm32/bare/01-blink/main.c
@@ -14,70 +14,137 @@
 
 
 // bit fields
-#define RCC_IOPBEN   (1<<3)
-#define RCC_IOPCEN   (1<<4)
-#define GPIO1       (1UL<<1)
-#define GPIO13      (1UL<<13)
+//#define RCC_IOPBEN   (1<<3)
+//#define RCC_IOPCEN   (1<<4)
+//#define GPIO1       (1UL<<1)
+//#define GPIO13      (1UL<<13)
 
 //static int j = 666; // this seems to cause problems
 
 // pins are defined as hex 0xPQ where P is port, and Q is pin number
 
 
+#define PA4  0x04
+#define PA5  0x05
+#define PA7  0x07
 #define PA13 0x0D
+#define PB0  0x10
 #define PB1  0x11
 #define PB13 0x1D
 #define PC13 0x2D
+#define PC15 0x2F
 
+#define BUILTIN_LED PC13
+
+void put32(u32 addr, u32 val)
+{
+	*(volatile u32*) addr = val;
+}
+
+u32 get32(u32 addr)
+{
+	return *(volatile u32*) addr;
+}
+
+u32 pin_to_gpio(u32 pin)
+{
+	u32 port = pin>>4;
+	return GPIO_BASE + port*0x400;
+}
+
+void gpio_write(u32 pin, u32 val)
+{
+	u32 pos = pin & 0xF;
+	u32 GPIO_BSRR = pin_to_gpio(pin) +  0x10;
+	if(val == 0 ) pos += 16; // Reset rather than Set
+	put32(GPIO_BSRR, 1<<pos);
+
+}
 void gpio_mode_out(u32 pin)
 {
 	u32 port = pin >>4; // gives 0 for Port A, 1 for port B, 2 for port C
+
+	// enable the port only if it not enabled
+	//u32 port_enabled = RCC_APB2ENR & (1 << (port+2));
+	//if(port_enabled == 0) 
 	RCC_APB2ENR |= (1 << (port+2)); // enable port
-	//GPIO_t* port_x = ((GPIO_t*) (GPIO_BASE + port*0x400));
-	u32 GPIOx_CRx = GPIO_BASE+port*0x400; // assume GPIOx_CRL
+
+	u32 GPIOx_CRx = pin_to_gpio(pin) + 0x00; // assume GPIOx_CRL
 	u32 pin_num = pin & 0x0F;
 	if(pin_num > 7) {
 		GPIOx_CRx += 0x04; // bump to GPIOx_CRH;
 		pin_num -= 8;
 	}
-	//GPIOx_CRx =  GPIO_BASE + 0x800 + 0x04;
 	u32 mask = 0b1111 << (pin_num*4);
 	*(volatile u32*) GPIOx_CRx &= ~mask; // mask out the mode and CNF
 	*(volatile u32*) GPIOx_CRx |= (0b0010<< (pin_num*4)) ; //  CNF output push-pull, max speed 2MHz
 	//GPIOC->CRH   |= 0x00200000;
+	//gpio_write(pin, 0);
 
 }
 
 
+void gpio_toggle(u32 pin)
+{
+	u32 GPIO_ODR = pin_to_gpio(pin) + 0x0C;
+	u32 on = get32(GPIO_ODR) &  (1<<(pin & 0xF));
+	if(on)
+		gpio_write(pin, 0);
+	else
+		gpio_write(pin, 1);
+}
+
 void main(void)
 {
-	gpio_mode_out(PC13);
+	//
+	//gpio_write(PC13, 0);
 
 	//RCC_APB2ENR |= RCC_IOPCEN; // enable port C
 	//GPIOC->CRH   &= 0xFF0FFFFF;
 	//GPIOC->CRH   |= 0x00200000;
 
 	// Set PB13 into a high output state
-	RCC_APB2ENR |= RCC_IOPBEN; // enable port B
+	//RCC_APB2ENR |= RCC_IOPBEN; // enable port B
 	// set PB13 for output
-	GPIOB->CRH   &= 0xFF0FFFFF;
-	GPIOB->CRH   |= 0x00200000;
+	//GPIOB->CRH   &= 0xFF0FFFFF;
+	//GPIOB->CRH   |= 0x00200000;
 	// set pin PB13 high
-	GPIOB->ODR |= GPIO13;
+	//GPIOB->ODR |= GPIO13;
+	gpio_mode_out(PB13);
+	//gpio_write(PB13, 1);
+	//gpio_write(PB13, 0);
 
 	// set PB1 for output and set it high
-	GPIOB->CRL  &= 0xFFFFFF0F;
-	GPIOB->CRL  |= 0x00000020;
-	GPIOB->ODR  |= GPIO1;
+	//GPIOB->CRL  &= 0xFFFFFF0F;
+	//GPIOB->CRL  |= 0x00000020;
+	//GPIOB->ODR  |= GPIO1;
+	gpio_mode_out(PB1);
+
+	gpio_mode_out(PC15);
+	//gpio_write(PC15, 1);
+	gpio_mode_out(BUILTIN_LED);
+	gpio_write(BUILTIN_LED, 1); // builtin LED works the opposite way you expect
 
 
 	int i;
 	while(1)
 	{
 		//GPIOC_ODR |=  GPIO13;
-		GPIOC->ODR |= GPIO13;
+		//GPIOC->ODR |= GPIO13;
+		//gpio_write(BUILTIN_LED, 1);
+		//gpio_write(PB1, 1);
+		gpio_toggle(PB1);
+		gpio_toggle(PC15);
+		gpio_write(BUILTIN_LED, 1);
+
+		gpio_write(PB13, 1);
 		for (i = 0; i < 500000; i++); // arbitrary delay
-		GPIOC->ODR &= ~GPIO13;
+		//GPIOC->ODR &= ~GPIO13;
+		//GPIOC->ODR ^= (1<<13);
+		//GPIOC->ODR = 0;
+		gpio_write(BUILTIN_LED, 0);
+		gpio_write(PB13, 0);
+		gpio_toggle(PB1);
 		for (i = 0; i < 500000; i++); // arbitrary delay
 	}
 }
