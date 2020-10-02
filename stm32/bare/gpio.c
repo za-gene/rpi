4fef08cee00cc4b01abb31aa59d563fc0b1d38c6
Mark Carter <alt.mcarter@gmail.com>
2020-10-02 12:26:44 +0100

    gpio fix

--- stm32/bare/gpio.c
+++ stm32/bare/gpio.c
@@ -1,18 +1,5 @@
 #include "gpio.h"
 
-#if 0
-void put32(u32 addr, u32 val)
-{
-	*(volatile u32*) addr = val;
-}
-
-u32 get32(u32 addr)
-{
-	return *(volatile u32*) addr;
-}
-#endif
-
-
 u32 pin_to_gpio(u32 pin)
 {
 	u32 port = pin>>4;
@@ -27,13 +14,11 @@ void gpio_write(u32 pin, u32 val)
 	put32(GPIO_BSRR, 1<<pos);
 
 }
-void gpio_mode_out(u32 pin)
+
+void gpio_mode(u32 pin, u8 mode)
 {
 	u32 port = pin >>4; // gives 0 for Port A, 1 for port B, 2 for port C
 
-	// enable the port only if it not enabled
-	//u32 port_enabled = RCC_APB2ENR & (1 << (port+2));
-	//if(port_enabled == 0) 
 	RCC_APB2ENR |= (1 << (port+2)); // enable port
 
 	u32 GPIOx_CRx = pin_to_gpio(pin) + 0x00; // assume GPIOx_CRL
@@ -44,10 +29,12 @@ void gpio_mode_out(u32 pin)
 	}
 	u32 mask = 0b1111 << (pin_num*4);
 	*(volatile u32*) GPIOx_CRx &= ~mask; // mask out the mode and CNF
-	*(volatile u32*) GPIOx_CRx |= (0b0010<< (pin_num*4)) ; //  CNF output push-pull, max speed 2MHz
-	//GPIOC->CRH   |= 0x00200000;
-	//gpio_write(pin, 0);
+	*(volatile u32*) GPIOx_CRx |= (((u32)mode)<< (pin_num*4)); 
+}
 
+void gpio_mode_out(u32 pin)
+{
+	gpio_mode(pin, OUTPUT);
 }
 
 
--- stm32/bare/gpio.h
+++ stm32/bare/gpio.h
@@ -30,8 +30,13 @@
 
 #define BUILTIN_LED PC13
 
+// section 9.2.1 and 9.2.2 
+#define INPUT  		0b0100 // CNF=1, MODE=0, floating input
+#define INPUT_PULLUP 	0b1000 // CNF=2, MODE =0. input with pull-up/down
+#define OUTPUT 		0b0001 // CNF=2, MODE=1, output 10MHz push-pull ("regular" out)
 
-//u32 pin_to_gpio(u32 pin);
+u32 pin_to_gpio(u32 pin);
 void gpio_write(u32 pin, u32 val);
+void gpio_mode(u32 pin, u8 mode);
 void gpio_mode_out(u32 pin);
 void gpio_toggle(u32 pin);
