ac6d9862eb6713ee6d14e851afbb302491c8993b
Mark Carter <alt.mcarter@gmail.com>
2020-10-18 18:59:16 +0100

    pwm cleanup

--- stm32/bare/11-pwm/main.c
+++ stm32/bare/11-pwm/main.c
@@ -30,7 +30,6 @@ void print_bin(u32 v)
 void setup_timer(u32 freq, u32 duty_pc)
 {
 	RCC_APB1ENR |= RCC_APB1ENR_TIM3EN;
-	RCC_APB2ENR |= RCC_APB2ENR_AFIOEN; // dunno if needed
 	gpio_mode(PA6, 0b1011); // output 50MHz, push-pull, alt function
 
 	// figure out prescaler and array
@@ -43,13 +42,11 @@ void setup_timer(u32 freq, u32 duty_pc)
 	}
 	TIM3->PSC = psc -1;
 	TIM3->ARR = arr -1;
+
 	TIM3->CCR1 = arr * duty_pc / 100 -1;
 	TIM3->CCER |= TIM_CCER_CC1E; // enable capture/compare 
 	TIM3->CCMR1 |= 0b110<<4; // output pwm compare mode 1 
 	TIM3->CR1 |= TIM_CR1_CEN; // enable counter
-
-	TIM3->EGR |= (TIM_EGR_TG | TIM_EGR_UG);
-	TIM3->CR1 |= TIM_CR1_CEN;
 }
 
 
@@ -65,7 +62,7 @@ void main()
 	init_serial();
 	puts("11 pwm");
 
-	setup_timer(440, 35);
+	setup_timer(440, 25);
 
 	printn(TIM3->PSC);
 	printn(TIM3->ARR);
