a122520aacd5261e05042f19366eb52b388346c5
Mark Carter <alt.mcarter@gmail.com>
2020-10-20 23:13:39 +0100

    dac through pwm much improved

--- stm32/bare/13-track/main.c
+++ stm32/bare/13-track/main.c
@@ -33,8 +33,9 @@ void TIM3_IRQHandler()
 {
 	// increment the PWM counter for when to set low
 	static u32 idx=0;
-	if(idx >= track_raw_len) idx = 0;
-	TIM3->CCR1 = (u32) track_raw[idx++]/2;
+	if(idx >= 2*track_raw_len) idx = 0;
+	TIM3->CCR1 = (u32) track_raw[idx/2];
+	idx++;
 
 	TIM3->SR &= ~TIM_SR1_UIF; // reset interrupt flag
 }
@@ -55,7 +56,7 @@ void setup_timer()
 
 	TIM3->ARR = 255;
 	//TIM3->PSC = 8000000UL/FREQ/NSAMPLES/NSAMPLES ;
-	TIM3->PSC = 3;
+	TIM3->PSC = 7;
 	printi(TIM3->PSC);
 
 
@@ -74,8 +75,17 @@ void printn(u32 v)
 }
 
 
+#define RCC_CR_PLLON (1<<24)
+#define RCC_CR_PLLRDY (1<<25)
+
 void main() 
 {
+	//RCC_CFGR |= 0b0101 << 24; // MCO to PLL
+	RCC_CFGR |= 0b0110 << 18; // PLLMUL 8X
+	RCC_CFGR |= 0b10;  //PLL selected as system clock
+	while(RCC_CR & RCC_CR_PLLRDY);
+	RCC_CR |= RCC_CR_PLLON;
+
 	gpio_mode_out(BUILTIN_LED);
 	init_serial();
 	puts("12 pwm with interrupt");
