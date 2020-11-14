cc08181a2ad7b3ed4be3a5d38efa67f4cc11a32a
Mark Carter <alt.mcarter@gmail.com>
2020-11-14 17:23:16 +0000

    Updated crunky

--- crunky/lfb.c
+++ crunky/lfb.c
@@ -62,9 +62,8 @@ extern volatile unsigned char _binary_font_sfn_start;
 unsigned int width, height, pitch, isrgb;   /* dimensions and channel order */
 unsigned char *lfb;                         /* raw frame buffer address */
 
-/**
- * Set screen resolution to 1024x768
- */
+#define WIDTH 1920
+#define HEIGHT 1060
 void lfb_init()
 {
     mbox[0] = 35*4;
@@ -73,14 +72,14 @@ void lfb_init()
     mbox[2] = 0x48003;  //set phy wh
     mbox[3] = 8;
     mbox[4] = 8;
-    mbox[5] = 1024;         //FrameBufferInfo.width
-    mbox[6] = 768;          //FrameBufferInfo.height
+    mbox[5] = WIDTH;         //FrameBufferInfo.width
+    mbox[6] = HEIGHT;          //FrameBufferInfo.height
 
     mbox[7] = 0x48004;  //set virt wh
     mbox[8] = 8;
     mbox[9] = 8;
-    mbox[10] = 1024;        //FrameBufferInfo.virtual_width
-    mbox[11] = 768;         //FrameBufferInfo.virtual_height
+    mbox[10] = WIDTH;        //FrameBufferInfo.virtual_width
+    mbox[11] = HEIGHT;         //FrameBufferInfo.virtual_height
 
     mbox[12] = 0x48009; //set virt offset
     mbox[13] = 8;
--- crunky/memory.h
+++ crunky/memory.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stddef.h>
+
 void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void* s, int c, size_t n);
 
