fab3b42b0132fb94e514a092a137644dfde50a2a
Mark Carter <alt.mcarter@gmail.com>
2020-11-25 22:25:02 +0000

    uart crunky stuff

--- crunky/examples/05a-uart0/kernel.c
+++ crunky/examples/05a-uart0/kernel.c
@@ -24,7 +24,7 @@
  */
 
 #include "uart.h"
-#include "lfb.h"
+#include "../../lfb.h"
 
 void kernel_main()
 {
@@ -33,13 +33,16 @@ void kernel_main()
     lfb_init();
 
     // display an ASCII string on screen with PSF
-    lfb_print(80, 80, "Hello World!");
+    //lfb_print(80, 80, "Hello World!");
 
     // display a UTF-8 string on screen with SSFN
-    lfb_proprint(80, 120, "Hello 多种语言 Многоязычный többnyelvű World!");
+    //lfb_proprint(80, 120, "Hello 多种语言 Многоязычный többnyelvű World!");
+    fbputs("i'll now echo back everything you type");
 
     // echo everything back
     while(1) {
-        uart0_send(uart0_getc());
+	    int c = uart0_getc();
+	    fbputchar(c);
+    	    uart0_send(c);
     }
 }
