344fae9d09b4546148f008410b1fbfd36c081042
Mark Carter <alt.mcarter@gmail.com>
2020-11-28 08:39:08 +0000

    Slight change to uart example

--- crunky/examples/05a-uart0/kernel.c
+++ crunky/examples/05a-uart0/kernel.c
@@ -28,16 +28,20 @@
 
 void kernel_main()
 {
+    char msg[] = "I'll now echo back everything you type";
+
     lfb_init();
+    fbputs(msg);
 
     // display an ASCII string on screen with PSF
     //lfb_print(80, 80, "Hello World!");
 
     // display a UTF-8 string on screen with SSFN
     //lfb_proprint(80, 120, "Hello 多种语言 Многоязычный többnyelvű World!");
-    fbputs("i'll now echo back everything you type");
+
 
     uart0_init();
+    uart0_puts(msg);
 
     // echo everything back
     while(1) {
