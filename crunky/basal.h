9cde1e5c81810de1e38781240c227b373706d228
Mark Carter <alt.mcarter@gmail.com>
2020-12-02 20:22:53 +0000

    tidy up of basal.h

--- crunky/basal.h
+++ crunky/basal.h
@@ -1,10 +1,10 @@
 #pragma once
 
 /**
- * RPi 0  : BCM2835. 1 core @ 1GHz. 512MB RAM.
- * RPi B+ : BCM2835
- * RPi 2  : BCM2836 quad core @ 1GHz. 1GB RAM.
- * RPi 3  : BCM2837
+ * RPi 0        : BCM2835. 1 core @ 1GHz. 512MB RAM.
+ * RPi A, B, B+ : BCM2835
+ * RPi 2        : BCM2836 quad core @ 1GHz. 1GB RAM.
+ * RPi 3        : BCM2837
  * RPi 3A+, 3B+ : BCM2837B0
  *
  * Datasheets (BCM*) can be found at
@@ -21,11 +21,6 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-
-//u64 __aeabi_uidivmod(u32 value, u32 divisor);
-//u32 __aeabi_uidiv(u32 value, u32 divisor);
-
-//#define RPI 3
 
 #define GPU_MEM_BASE  0xC0000000 // L2 cache disabled, depending on Pi
 // 0x40000000 on the Pi Zero, Pi Zero W, and the first generation of the Raspberry Pi 
