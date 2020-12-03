55b177e4fbf2eef665ab73c6fdfad87c7ec47398
Mark Carter <alt.mcarter@gmail.com>
2020-12-03 14:34:06 +0000

    tweak to delay

--- crunky/examples/01-blink/kernel.c
+++ crunky/examples/01-blink/kernel.c
@@ -6,10 +6,11 @@ void kernel_main(void)
 {
 	const int bcm_pin = 26;
 	gpio_sel(bcm_pin, OUTPUT); // set its pin mode to OUTPUT
+	u32 delay = 5;
 	while(1) {
 		gpio_set(bcm_pin); // set the pin high
-		delay_ms(10);  
+		delay_ms(delay);  
 		gpio_clr(bcm_pin); // turn the pin off
-		delay_ms(10);
+		delay_ms(delay);
 	}
 }
