6452848b47c8c50673a6d245666b21e4246b4ff4
Mark Carter <alt.mcarter@gmail.com>
2020-12-05 12:38:11 +0000

    modified:   spi/rpi-master-spi.py
    it now works with our standard SPI slave

--- spi/rpi-master-spi.py
+++ spi/rpi-master-spi.py
@@ -1,12 +1,27 @@
-import spidev
+import spidev, time
 spi = spidev.SpiDev()
-#spi.open(0, CHIP_SELECT_0_OR_1)
-spi.open(0, 0)
-#spi.max_speed_hz = 1000000
-spi.max_speed_hz = 10000
-to_send = [0x01, 0x02]
-#x = spi.xfer(to_send)
-#print(x)
-hi, lo = spi.readbytes(2)
-val = (hi << 8) + lo
-print(val)
+
+# notes about transferring multiple values
+# to_send = [0x01, 0x02]
+# hi, lo = spi.readbytes(2)
+# val = (hi << 8) + lo
+# print(val)
+
+def loop():
+    x = spi.xfer([0])
+    print(x)
+    time.sleep(1.0)
+
+
+def main():
+    global spi
+    try:
+        spi.open(0, 0)
+        spi.max_speed_hz = 100_000
+        while True:
+            loop()
+    finally:
+        spi.close()
+
+if __name__ == "__main__":
+    main()
