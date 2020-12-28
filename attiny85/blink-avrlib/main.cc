b8dca217eddf16c925c069df1242aabcf9bd22e9
Mark Carter <alt.mcarter@gmail.com>
2020-12-28 12:11:52 +0000

    improved blink sketch
    Also fixed subtle bug

--- attiny85/blink-avrlib/main.cc
+++ attiny85/blink-avrlib/main.cc
@@ -4,12 +4,18 @@
 #include <util/delay.h>
 
 
-#define LED 1<<PB4 // physical pin 3
+#define LED (1<<PB4) // physical pin 3
 
 int main()
 {
 	DDRB = LED; // set PB4 for output
 	PORTB = 0x00;  // set all pins low
+
+	// experiment to see how many amps a pin has
+	DDRB  |= 1<<PB3; // physical pin 2 is output
+	PORTB |= 1<<PB3; // set output high
+
+	//while(1);
 
 	for(;;) {
 		PORTB |= LED;
