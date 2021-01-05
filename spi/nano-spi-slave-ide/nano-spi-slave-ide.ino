1d45c7c248900ce2c0c361a805fbfee726933e20
Mark Carter <alt.mcarter@gmail.com>
2021-01-05 18:33:28 +0000

    nano spi stuff

--- spi/nano-spi-slave-ide/nano-spi-slave-ide.ino
+++ spi/nano-spi-slave-ide/nano-spi-slave-ide.ino
@@ -1,9 +1,10 @@
 #include<SPI.h>
 
 void setup() {
-  pinMode(MISO, OUTPUT);                  //Sets MISO as OUTPUT (Have to Send data to Master IN
-  SPCR |= _BV(SPE);                       //Turn on SPI in Slave Mode
-  SPI.attachInterrupt();                  //Interuupt ON is set for SPI commnucation
+  pinMode(MISO, OUTPUT);  //Sets MISO as OUTPUT (Have to Send data to Master IN
+  SPCR |= _BV(SPE);       //Turn on SPI in Slave Mode
+  SPI.attachInterrupt();  //Interuupt ON is set for SPI commnucation
+  Serial.begin(115200);
 }
 
 
@@ -12,7 +13,9 @@ volatile uint8_t cnt = 0;
 ISR(SPI_STC_vect)
 {
   byte c = SPDR; //received
-  SPDR = cnt++;
+  SPDR = cnt;
+  Serial.println(cnt);
+  cnt++;
 }
 
 void loop(){}
