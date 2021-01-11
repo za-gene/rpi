38724d0e167cb1e580d744817faf1ec65e3f162e
Mark Carter <alt.mcarter@gmail.com>
2021-01-11 22:21:13 +0000

    Clock setting added

---
+++ 0clock/settime/clock.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timezone
+
+import SDL_DS3231
+
+ds = SDL_DS3231.SDL_DS3231()
+print(ds.read_str());
+now = datetime.now(tz=timezone.utc)
+print(now)
+ds.write_datetime(now)
+print(ds.read_str());
+
+#print(datetime.fromtimestamp(now, tz=timezone.utc).isoformat())
+
