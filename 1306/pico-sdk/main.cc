b6a0aab1630e666096603056ed1e14200e38b1a4
Mark Carter <alt.mcarter@gmail.com>
2021-01-24 19:43:52 +0000

    Much improvement in 1306

--- 1306/pico-sdk/main.cc
+++ 1306/pico-sdk/main.cc
@@ -33,13 +33,13 @@ typedef uint8_t u8;
 #define I2C_PORT i2c0
 #define SID 0x3C // I2C 1306 slave ID ... for 64 height display. 32 height is 0x3D
 
-const u8 height = 128;
-const u8 width = 64;
+const u8 height = 64;
+const u8 width = 128;
 const int pages = height / 8;
 const bool external_vcc = false;
 
 
-u8 scr[height*width/8];
+u8 scr[pages*width+1]; // extra byte holds data send instruction
 
 void write_cmd(u8 cmd);
 
@@ -49,6 +49,13 @@ void fill_scr(u8 v)
 }
 
 
+void send2(u8 v1, u8 v2)
+{
+	u8 buf[2];
+	buf[0] = v1;
+	buf[1] = v2;
+	i2c_write_blocking(I2C_PORT, SID, buf, 2, false);
+}
 
 /* Original Python
    def show(self):
@@ -69,42 +76,43 @@ self.write_data(self.buffer)
 
 void show_scr()
 {
+#if 0
 	int x0 = 0;
 	int x1 = width -1;
 	if(width == 64) { x0 += 32; x1 += 32; } // displays of width 64 are shifted by 32
-	write_cmd(SET_COL_ADDR);
+	write_cmd(SET_COL_ADDR); // 0x21
 	write_cmd(x0);
 	write_cmd(x1);
-	write_cmd(SET_PAGE_ADDR);
-	write_cmd(0);
-	write_cmd(pages - 1);
-
-	// write screen data itself
-	// very inefficient
-	u8 buf[sizeof(scr)+1];
-	buf[0] = 0x40;
-	memcpy(buf+1, scr, sizeof(scr));
-	i2c_write_blocking(I2C_PORT, SID, buf, sizeof(buf), false);
-}
-
-
-#if 0
-void send2(u8 v1, u8 v2)
-{
-	u8 buf[2];
-	buf[0] = v1;
-	buf[1] = v2;
-	i2c_write_blocking(I2C_PORT, SID, buf, 2, false);
-}
 #endif
+
+	write_cmd(SET_MEM_ADDR); // 0x20
+	write_cmd(0b01); // vertical addressing mode
+
+	write_cmd(SET_COL_ADDR); // 0x21
+	write_cmd(0);
+	write_cmd(127);
+
+	write_cmd(SET_PAGE_ADDR); // 0x22
+	write_cmd(0);
+	write_cmd(7);
+
+
+	scr[0] = 0x40; // the data instruction	
+	i2c_write_blocking(I2C_PORT, SID, scr, sizeof(scr), false);
+}
+
+
 
 void write_cmd(u8 cmd) 
 { 
+	send2(0x80, cmd);
+#if 0
 	u8 buf[2];
 	buf[0] = 0x80;
 	buf[1] = cmd;
 	i2c_write_blocking(I2C_PORT, SID, buf, 2, false);
 	//send2(0x80, cmd); 
+#endif
 }
 
 
@@ -134,38 +142,50 @@ void invert(u8 invert) { write_cmd(SET_NORM_INV | (invert & 1)); }
 void init_display()
 {
 	u8 cmds[] = {
-		SET_DISP | 0x00,  //# off
-		//# address setting
-		SET_MEM_ADDR,
-		0x00,  //# horizontal
+		SET_DISP | 0x00,  // display off 0x0E | 0x00
+
+		SET_MEM_ADDR, // 0x20
+		0x00,  // horizontal
+
 		//# resolution and layout
-		SET_DISP_START_LINE | 0x00,
+		SET_DISP_START_LINE | 0x00, // 0x40
 		SET_SEG_REMAP | 0x01,  //# column addr 127 mapped to SEG0
-		SET_MUX_RATIO,
+
+		SET_MUX_RATIO, // 0xA8
 		height - 1,
-		SET_COM_OUT_DIR | 0x08,  //# scan from COM[N] to COM0
-		SET_DISP_OFFSET,
+
+		SET_COM_OUT_DIR | 0x08,  //# scan from COM[N] to COM0  (0xC0 | val)
+		SET_DISP_OFFSET, // 0xD3
 		0x00,
-		SET_COM_PIN_CFG,
+
+		//SET_COM_PIN_CFG, // 0xDA
 		//0x02 if self.width > 2 * self.height else 0x12,
-		width > 2*height ? 0x02 : 0x12,
+		//width > 2*height ? 0x02 : 0x12,
+		//SET_COM_PIN_CFG, height == 32 ? 0x02 : 0x12,
+
 		//# timing and driving scheme
-		SET_DISP_CLK_DIV,
+		SET_DISP_CLK_DIV, // 0xD5
 		0x80,
+
 		SET_PRECHARGE,
 		//0x22 if self.external_vcc else 0xF1,
 		external_vcc ? 0x22 : 0xF1,
+
 		SET_VCOM_DESEL,
-		0x30,  //# 0.83*Vcc
+		//0x30,  //# 0.83*Vcc
+		0x40, // changed by mcarter
+
 		//# display
-		SET_CONTRAST,
+		SET_CONTRAST, // 0x81
 		0xFF,  //# maximum
-		SET_ENTIRE_ON,  //# output follows RAM contents
-		SET_NORM_INV,  //# not inverted
-		//# charge pump
-		SET_CHARGE_PUMP,
+
+		SET_ENTIRE_ON,  //# output follows RAM contents // 0xA4
+		SET_NORM_INV,  //# not inverted 0xA6
+
+		SET_CHARGE_PUMP, // 0x8D
 		//0x10 if self.external_vcc else 0x14,
 		external_vcc ? 0x10 : 0x14,
+
 		SET_DISP | 0x01
 	};
 
@@ -218,16 +238,19 @@ void draw_pixel(int16_t x, int16_t y, int color)
 {
 	if(x<0 || x >= width || y<0 || y>= height) return;
 
-	int offset = x +(y/8)*width;
+	int page = y/8;
+	int bit = 1<<(y % 8);
+	u8* ptr = scr + x*8 + page  + 1; 
+
 	switch (color) {
 		case 1: // white
-			scr[offset] |= (1 << (y & 7));
+			*ptr |= bit;
 			break;
 		case 0: // black
-			scr[offset] &= ~(1 << (y & 7));
+			*ptr &= ~bit;
 			break;
 		case -1: //inverse
-			scr[offset] ^= (1 << (y & 7));
+			*ptr ^= bit;
 			break;
 	}
 
@@ -256,13 +279,14 @@ u8 letterP[] = {
 	0b00000000
 };
 
-void draw_letter(u8 letter[]) {
+void draw_letter_at(u8 x, u8 y, u8 letter[])
+{
 	//clear1306();
 	for (int i = 0; i < 8; i++) {
 		u8 row = letter[i];
 		for (int j = 0; j < 8; j++) {
 			u8 on = (row & 1 << 7) ? 1 : 0;
-			draw_pixel(j, i, on);
+			draw_pixel(j+x, i+y, on);
 			row <<= 1;
 		}
 	}
@@ -270,23 +294,45 @@ void draw_letter(u8 letter[]) {
 	//delayish(2000);
 }
 
+void draw_letter(u8 letter[]) { draw_letter_at(0, 0, letter); }
+void pixel(int x, int y)
+{
+	//int x = 100, y=50;
+	int page = y/8;
+	u8 patt = 1<<(y%8);
+	//scr[1+ 0] |= 0xff;
+	scr[1+ x*8 + page] |= patt;
+	
+}
 void once()
 {
-	init_i2c();
-	init_display();
 	fill_scr(0);
-	draw_letter(letterH);
+	draw_letter_at(0,0, letterH);
+	draw_letter_at(60,0, letterP);
+	draw_letter_at(60,30, letterP);
 #if 0
 	for(int x = 0; x< 10; x++) 
-	for(int y = 0; y< 20; y++) 
-		draw_pixel(x,y, 1);
+		for(int y = 0; y< 30; y++) 
+			draw_pixel(y+1, x+1, 1);
 #endif
+	for(int i = 0; i< 60; i++) pixel(i, i);
 	show_scr();
 	sleep_ms(500);
 }
 
 int main()
 {
-	for(;;) once();
+	init_i2c();
+	init_display();
+	once();
+	if(0) {
+		for(int i = 0; i<64; i++) {
+			draw_pixel(i, i, 1);
+			draw_pixel(i, 63-i, 1);
+		}
+		show_scr();
+	}
+
+	for(;;) sleep_ms(500);
 	return 0;
 }
