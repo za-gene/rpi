f4a99f142f75ff7ac03cf1c400d04e9dce545753
Mark Carter <alt.mcarter@gmail.com>
2021-01-29 10:08:29 +0000

    picol now works

--- pico/picol/picol.c
+++ pico/picol/picol.c
@@ -554,30 +554,34 @@ void picolRegisterCoreCommands(struct picolInterp *i) {
 	picolRegisterCommand(i,"return",picolCommandReturn,NULL);
 }
 
-int main(int argc, char **argv) {
+char* echoed_fgets(char* s, int size, FILE *stream)
+{
+	int i=0;
+	while(i <size) {
+		int c = fgetc(stream);
+		if(c == '\r') continue;
+		putchar(c);
+		s[i++] = (char) c;
+		if(c == '\n') break;
+	}
+	s[i] = 0;
+	return s;
+}
+
+
+int main() {
 	stdio_init_all();
 	struct picolInterp interp;
 	picolInitInterp(&interp);
 	picolRegisterCoreCommands(&interp);
-	if (argc == 1) {
-		while(1) {
-			char clibuf[1024];
-			int retcode;
-			printf("picol> "); fflush(stdout);
-			if (fgets(clibuf,1024,stdin) == NULL) return 0;
-			retcode = picolEval(&interp,clibuf);
-			if (interp.result[0] != '\0')
-				printf("[%d] %s\n", retcode, interp.result);
-		}
-	} else if (argc == 2) {
-		char buf[1024*16];
-		FILE *fp = fopen(argv[1],"r");
-		if (!fp) {
-			perror("open"); exit(1);
-		}
-		buf[fread(buf,1,1024*16,fp)] = '\0';
-		fclose(fp);
-		if (picolEval(&interp,buf) != PICOL_OK) printf("%s\n", interp.result);
+	while(1) {
+		char clibuf[1024];
+		int retcode;
+		printf("picol> "); fflush(stdout);
+		if (echoed_fgets(clibuf,1024,stdin) == NULL) return 0;
+		retcode = picolEval(&interp,clibuf);
+		if (interp.result[0] != '\0')
+			printf("[%d] %s\n", retcode, interp.result);
 	}
 	return 0;
 }
