ed0e22bd6acd771d26c0a7ff51db5f2cff53cc59
Mark Carter <alt.mcarter@gmail.com>
2021-02-01 20:06:26 +0000

    cleanup of pico 1306

--- 1306/pico-sdk/main.cc
+++ 1306/pico-sdk/main.cc
@@ -5,12 +5,6 @@
 
 extern "C" const uint8_t ssd1306_font6x8[];
 
-//# MicroPython SSD1306 OLED driver, I2C and SPI interfaces
-
-//from micropython import const
-//import framebuf
-
-
 #define SET_CONTRAST 0x81
 #define SET_ENTIRE_ON 0xA4
 #define SET_NORM_INV 0xA6
@@ -58,33 +52,9 @@ void send2(u8 v1, u8 v2)
 	i2c_write_blocking(I2C_PORT, SID, buf, 2, false);
 }
 
-/* Original Python
-   def show(self):
-   x0 = 0
-   x1 = self.width - 1
-   if self.width == 64:
-# displays with width of 64 pixels are shifted by 32
-x0 += 32
-x1 += 32
-self.write_cmd(SET_COL_ADDR)
-self.write_cmd(x0)
-self.write_cmd(x1)
-self.write_cmd(SET_PAGE_ADDR)
-self.write_cmd(0)
-self.write_cmd(self.pages - 1)
-self.write_data(self.buffer)
-*/
 
 void show_scr()
 {
-#if 0
-	int x0 = 0;
-	int x1 = width -1;
-	if(width == 64) { x0 += 32; x1 += 32; } // displays of width 64 are shifted by 32
-	write_cmd(SET_COL_ADDR); // 0x21
-	write_cmd(x0);
-	write_cmd(x1);
-#endif
 
 	write_cmd(SET_MEM_ADDR); // 0x20
 	write_cmd(0b01); // vertical addressing mode
@@ -107,29 +77,9 @@ void show_scr()
 void write_cmd(u8 cmd) 
 { 
 	send2(0x80, cmd);
-#if 0
-	u8 buf[2];
-	buf[0] = 0x80;
-	buf[1] = cmd;
-	i2c_write_blocking(I2C_PORT, SID, buf, 2, false);
-	//send2(0x80, cmd); 
-#endif
 }
 
 
-#if 0
-/* Original Python code:
-   def write_cmd(self, cmd):
-   self.temp[0] = 0x80  # Co=1, D/C#=0
-   self.temp[1] = cmd
-   self.i2c.writeto(self.addr, self.temp)
-   */
-void write_cmds(u8* cmds, int n)
-{
-	for(int i=0; i<n; i++)
-		write_cmd(cmds[i]);
-}
-#endif
 
 void poweroff() { write_cmd(SET_DISP | 0x00); }
 
@@ -198,33 +148,6 @@ void init_display()
 	show_scr();
 }
 
-/*
-# Subclassing FrameBuffer provides support for graphics primitives
-# http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
-class SSD1306(framebuf.FrameBuffer):
-def __init__(self, width, height, external_vcc):
-self.width = width
-self.height = height
-self.external_vcc = external_vcc
-self.pages = self.height // 8
-self.buffer = bytearray(self.pages * self.width)
-super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
-self.init_display()
-
-
-class SSD1306_I2C(SSD1306):
-def __init__(self, width, height, i2c, addr=0x3C, external_vcc=False):
-self.i2c = i2c
-self.addr = addr
-self.temp = bytearray(2)
-self.write_list = [b"\x40", None]  # Co=0, D/C#=1
-super().__init__(width, height, external_vcc)
-
-def write_data(self, buf):
-self.write_list[1] = buf
-self.i2c.writevto(self.addr, self.write_list)
-*/
-
 void init_i2c()
 {
 	// This example will use I2C0 on GPIO4 (SDA) and GPIO5 (SCL)
@@ -261,28 +184,21 @@ void draw_pixel(int16_t x, int16_t y, int color)
 
 void draw_letter_at(u8 x, u8 y, char c)
 {
-	//char c = 'C';
-	//c= 'Q';
 	if(c< ' ' || c>  0x7F) c = '?'; // 0x7F is the DEL key
 
 	int offset = 4 + (c - ' ' )*6;
 	for(int col = 0 ; col < 6; col++) {
 		u8 line =  ssd1306_font6x8[offset+col];
 		for(int row =0; row <8; row++) {
-			//char x = (line & (1<<7)) ? 'X' : '_';
 			draw_pixel(x+col, y+row, line & 1);
-			//cout << x;
 			line >>= 1;
 		}
-		//cout << '\n';
 	}
 
-	//for(int row = 6; row<8; col++) {
 	for(int row = 0; row<8; row++) {
 		draw_pixel(x+6, y+row, 0);
 		draw_pixel(x+7, y+row, 0);
 	}
-	//cout << "________\n";
 
 }
 
@@ -290,10 +206,8 @@ void draw_letter_at(u8 x, u8 y, char c)
 void draw_letter(char c) { draw_letter_at(0, 0, c); }
 void pixel(int x, int y)
 {
-	//int x = 100, y=50;
 	int page = y/8;
 	u8 patt = 1<<(y%8);
-	//scr[1+ 0] |= 0xff;
 	scr[1+ x*8 + page] |= patt;
 
 }
@@ -303,11 +217,6 @@ void once()
 	draw_letter_at(0,0, 'P');
 	draw_letter_at(60,0, 'M');
 	draw_letter_at(60,30, 'C');
-#if 0
-	for(int x = 0; x< 10; x++) 
-		for(int y = 0; y< 30; y++) 
-			draw_pixel(y+1, x+1, 1);
-#endif
 	for(int i = 0; i< 60; i++) pixel(i, i);
 	show_scr();
 	sleep_ms(500);
@@ -335,7 +244,6 @@ int main()
 {
 	init_i2c();
 	init_display();
-	//once();
 	if(0) {
 		for(int i = 0; i<64; i++) {
 			draw_pixel(i, i, 1);
@@ -354,7 +262,6 @@ int main()
 		for(int x = 0; x < 128; x++) {
 			draw_pixel(x, y, 1);
 			if((update++ % 16) == 0)show_scr();
-			//sleep_ms(5);
 		}
 	}
 	show_scr();
