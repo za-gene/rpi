56b2375782c22468a6e0268a6f633dd0dae2659c
Mark Carter <alt.mcarter@gmail.com>
2021-02-01 20:35:41 +0000

    generalised pico1306.py

--- 1306/pico1306.py
+++ 1306/pico1306.py
@@ -5,15 +5,18 @@ import ssd1306
 from machine import I2C, Pin
 import utime
 
+width = 128
+height = 64
+
 sda = Pin(4) # physical pin 6
 scl = Pin(5) # physical pin 7
 i2c = I2C(id=0, sda=sda, scl=scl, freq=100000)
-ssd = ssd1306.SSD1306_I2C(128, 64, i2c)
+ssd = ssd1306.SSD1306_I2C(width, height, i2c)
 
 count =0
 def once():
     global count
-    ssd = ssd1306.SSD1306_I2C(128, 64, i2c)
+    ssd = ssd1306.SSD1306_I2C(width, height, i2c)
     ssd.fill(0)
     #ssd.rect(10,10, 10, 10, 1)
     #ssd.show()
