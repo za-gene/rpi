5fe5cb7853445d05232c5e3b3b37515db1981542
Mark Carter <alt.mcarter@gmail.com>
2021-02-02 19:44:02 +0000

    Vast improvement to the stm8 arduino oled port

--- 1306/stm8-1306-arduino/stm8-1306-arduino.ino
+++ 1306/stm8-1306-arduino/stm8-1306-arduino.ino
@@ -1,5 +1,5 @@
 /*
-   2020-10-12 under construction. It semi-works. Contents are nonsense.
+   2021-02-02 under construction, but working.
 */
 
 //#include "Wire8.h"
@@ -50,127 +50,122 @@ typedef uint32_t u32;
 #define SSD1306_SET_VERTICAL_SCROLL_AREA 0xA3             ///< Set scroll range
 
 
-#define HEIGHT  64
 #define WIDTH  128
-//u8 buffer1306[WIDTH * ((HEIGHT + 7) / 8)];
 
-#define SID 0x3C // I2C 1306 slave ID ... for 64 height display. 32 hieight is 0x3D
+#if 0
+  #define HEIGHT  32
+#else  
+  #define HEIGHT  64
+#endif
 
+
+#define SID 0x3C
+
+void draw_glyph(u8 n);
 
 void send2(u8 b1, u8 b2) {
   u8 buf = b2;
-  //buf[0] = b1;
-  //buf[1] = b2;
   I2C_write_sn(SID, b1, &buf, 1);
 }
 void ssd1306_command1(uint8_t c) {
-  /*
-  u8 buf[2];
-  buf[0] = 0;
-  buf[1] = c;
-  write_i2c(SID, buf, 2);
-  */
-  send2(0, c);
+  send2(0x80, c);
 }
 
-void send_u8_i2c(u8 c) {
-  u8 buff = c;
-  //send_i2c(&buff, 1);
-  I2C_write(SID, c);
-}
 
 void ssd1306_commandList(const uint8_t *c, uint8_t n) {
-  /*
-  begin_i2c(SID);
-  send_u8_i2c(0x00); // Co = 0, D/C = 0
-  while (n--) send_u8_i2c(*c++);
-  end_i2c();
-  */
-  I2C_write_sn(SID, 0x00, c, n);
+  for (int i = 0; i < n; i++) {
+    send2(0x80, c[i]);
+  }
 }
 
 
-bool init1306(uint8_t vcs) {
-  //clear1306();
-  u8 vccstate = vcs;
+void home()
+{
+  static const uint8_t  dlist1[] = {
+    SSD1306_PAGEADDR,
+    0,                      // Page start address
+    HEIGHT/8-1,                   // Page end (not really, but works here)
+    SSD1306_COLUMNADDR, 0, 127
+  };
+  ssd1306_commandList(dlist1, sizeof(dlist1));
+}
 
-  //i2caddr = addr ? addr : ((HEIGHT == 32) ? 0x3C : 0x3D);
-  //init_i2c();
+void clr_scr() {
+  home();
+  for(int i = 0; i< HEIGHT/8*128/5; i++)
+  draw_glyph(10);
+  
+}
 
-  uint8_t comPins = 0x02;
-  u8 contrast = 0x8F;
-
-  if ((WIDTH == 128) && (HEIGHT == 32)) {
-    comPins = 0x02;
-    contrast = 0x8F;
-  } else if ((WIDTH == 128) && (HEIGHT == 64)) {
-    comPins = 0x12;
-    contrast = (vccstate == SSD1306_EXTERNALVCC) ? 0x9F : 0xCF;
-  } else if ((WIDTH == 96) && (HEIGHT == 16)) {
-    comPins = 0x2; // ada x12
-    contrast = (vccstate == SSD1306_EXTERNALVCC) ? 0x10 : 0xAF;
-  } else {
-    // Other screen varieties -- TBD
-  }
-
-
+bool init1306() {
+  uint8_t comPins = 0x12;
+  if (HEIGHT == 32) comPins = 0x02;
 
   u8 init1[] = {
     SSD1306_DISPLAYOFF,         // 0xAE
-    SSD1306_SETDISPLAYCLOCKDIV, // 0xD5
-    0x80, // the suggested ratio 0x80
-    SSD1306_SETMULTIPLEX, // 0xA8
-    HEIGHT - 1,
-    SSD1306_SETDISPLAYOFFSET, // 0xD3
-    0x0,                      // no offset
+    SSD1306_SETDISPLAYCLOCKDIV, 0x80, // 0xD5
+    SSD1306_SETMULTIPLEX, HEIGHT - 1, // 0xA8
+    
+    SSD1306_SETDISPLAYOFFSET, 0x00, // 0xD3
     SSD1306_SETSTARTLINE | 0x0, // line #0
-    SSD1306_CHARGEPUMP,        // 0x8D
-    (vccstate == SSD1306_EXTERNALVCC) ? 0x10 : 0x14,
-    SSD1306_MEMORYMODE, // 0x20
-    0x00, // 0x0 act like ks0108
+    SSD1306_CHARGEPUMP, 0x14,       // 0x8D
+    SSD1306_MEMORYMODE, 0, // 0x20
     SSD1306_SEGREMAP | 0x1,
     SSD1306_COMSCANDEC,
     SSD1306_SETCOMPINS, comPins,
-    SSD1306_SETCONTRAST, contrast,
-    SSD1306_SETPRECHARGE, // 0xd9
-    (vccstate == SSD1306_EXTERNALVCC) ? 0x22 : 0xF1,
-    SSD1306_SETVCOMDETECT, // 0xDB
-    0x40,
+    SSD1306_SETCONTRAST, 0x7F,
+    SSD1306_SETPRECHARGE, 0xF1, // 0xd9
+    SSD1306_SETVCOMDETECT, 0x40, // 0xDB
     SSD1306_DISPLAYALLON_RESUME, // 0xA4
     SSD1306_NORMALDISPLAY,       // 0xA6
     SSD1306_DEACTIVATE_SCROLL,
     SSD1306_DISPLAYON
   };
   ssd1306_commandList(init1, sizeof(init1));
+  clr_scr();
+  home();
 
   return true;
 }
 
+uint8_t digital_font5x7_123[] =
+{
+  0x36, 0x41, 0x41, 0x36, 0x00, // char '0' (0x30/48)
+  0x00, 0x00, 0x00, 0x36, 0x00, // char '1' (0x31/49)
+  0x30, 0x49, 0x49, 0x06, 0x00, // char '2' (0x32/50)
+  0x00, 0x49, 0x49, 0x36, 0x00, // char '3' (0x33/51)
+  0x06, 0x08, 0x08, 0x36, 0x00, // char '4' (0x34/52)
+  0x06, 0x49, 0x49, 0x30, 0x00, // char '5' (0x35/53)
+  0x36, 0x49, 0x49, 0x30, 0x00, // char '6' (0x36/54)
+  0x00, 0x01, 0x01, 0x36, 0x00, // char '7' (0x37/55)
+  0x36, 0x49, 0x49, 0x36, 0x00, // char '8' (0x38/56)
+  0x06, 0x49, 0x49, 0x36, 0x00, // char '9' (0x39/57)
+  0x00, 0x00, 0x00, 0x00, 0x00, // 10 space
+};
 
+
+void send_data(u8* buf, int len)
+{
+  I2C_write_sn(SID, 0x40, buf, len);
+}
+
+void draw_glyph(u8 n) {
+  send_data(digital_font5x7_123 + n * 5, 5);
+}
 
 void  low_level_test() {
-  static const uint8_t  dlist1[] = {
-    SSD1306_PAGEADDR,
-    1,                      // Page start address
-    1,                   // Page end (not really, but works here)
-    SSD1306_COLUMNADDR, 0, 7
-  };
-  ssd1306_commandList(dlist1, sizeof(dlist1));
-  /*
-  begin_i2c(SID);
-  send_u8_i2c(0x40);
-  send_u8_i2c(0b10101010);
-  end_i2c();
-  */
-  send2(0x40, 0b10101010);
-  //ssd1306_command1(WIDTH - 1); // Column end address
+  home();
+ 
+  draw_glyph(9);
+  draw_glyph(1);
+  draw_glyph(2);
 }
 
 
 void setup() {
   //Wire.begin();
   I2C_begin();
-  init1306(SSD1306_SWITCHCAPVCC);
+  init1306();
   //I2C_end(); // dunno if needed
   low_level_test();
 
