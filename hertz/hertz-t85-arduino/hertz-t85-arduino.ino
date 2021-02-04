01d6758a715d27bd9c84306af4fc7762344d27a9
Mark Carter <alt.mcarter@gmail.com>
2021-02-04 19:59:28 +0000

    hertz t85 periodic timer ok

--- hertz/hertz-t85-arduino/hertz-t85-arduino.ino
+++ hertz/hertz-t85-arduino/hertz-t85-arduino.ino
@@ -1,19 +1,23 @@
+// vim: ft=cpp
+
 #define F_CPU 8000000
 
-//#include <tinySPI.h> // doesn't seem to work
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-/* use usual pins for SPI:
-   PB2 SCK
-   PB0 MOSI
-   MISO not required
-   Use 5V input to the display
- */
+
+typedef uint8_t u8; 
+//typedef uint16_t u16; 
+typedef uint32_t u32; 
+
+
+/////////////////////////////////////////////////////////////////////////
+// DISPLAY
 
 #define CS PB1
 #define MOSI PB0
 #define SCK PB2 // traditionally reserved for MISO, but we don't need it
 
-typedef uint32_t u32; 
 
 void transfer_7219(uint8_t address, uint8_t value) {
 	digitalWrite(CS, LOW); 
@@ -34,14 +38,14 @@ void init_7219() {
 	transfer_7219(0x0C, 0x01); // Turn on chip
 }
 
-void setup() {
+void init_display() {
 	CLKPR = 1<< CLKPCE; // tell the chip we're goint ot scale it
 	CLKPR = 0; // to 8MHz
 	init_7219();
 
 }
 
-void loop() {
+void display_count() {
 	static u32 cnt = 0; // "int" is too limiting
 	u32 num = cnt;
 	for (uint8_t i = 0; i < 8; ++i)
@@ -57,7 +61,7 @@ void loop() {
 		if((c==0) && (num==0)) { sep = 0; c = 0b1111; }
 
 		c |= sep;
-		
+
 		transfer_7219(i+1, c);
 		delay(1);
 	}
@@ -65,3 +69,78 @@ void loop() {
 	//delay(10);
 
 }
+
+/////////////////////////////////////////////////////////////////////////
+// FALLING
+
+#include "avr/interrupt.h"
+
+
+#define LED PB4
+
+void init_falling()
+{
+	// set up PB2 to detect falling edges
+	GIMSK |= (1<<6);	// INT0 bit
+	MCUCR |= 0b10;		// falling edge
+	sei();                 // enables interrupts
+
+	pinMode(LED, OUTPUT);
+}
+
+ISR(INT0_vect)
+{
+	static volatile int on = false;
+	on = 1 - on;	
+	digitalWrite(LED, on);
+}
+
+/////////////////////////////////////////////////////////////////////////
+// TIMER1
+
+
+#define LED  PB4
+
+
+void init_timer1(unsigned long freq)
+{
+	TCCR1 = 0; // stop the counter
+	TCNT1 = 0;
+	OCR1C = 0;
+	if(freq==0) return;
+
+	int prescale = 1;
+	while(freq * 256 * (1<<(prescale-1)) < F_CPU)
+		prescale++;
+	TCCR1 = (1<<CTC1);
+	TCCR1 |= prescale;
+	int divisor = F_CPU/(1<<(prescale-1))/freq;
+	OCR1C = divisor; // compare match value
+	TIMSK |= (1<<OCIE1A); // enable compare match interrupt
+	sei();
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	static volatile u16 cnt = 0;
+	//cnt++;
+	if(++cnt % 1000 != 0) return;
+	cnt = 0;
+
+	digitalWrite(LED, 1 - digitalRead(LED));
+}
+
+
+/////////////////////////////////////////////////////////////////////////
+// ENSEMBLE
+
+void setup() {
+	init_display();
+	init_falling();
+	init_timer1(1000); // use 1000Hz because there is a minumum
+	sei();
+}
+
+void loop() {
+}
+
