4508601094dc1079d49816c69cfa589a4e967104
Mark Carter <alt.mcarter@gmail.com>
2021-02-14 18:35:56 +0000

    tidied up sd-card test

--- crunky/examples/09-sdcard/kernel.c
+++ crunky/examples/09-sdcard/kernel.c
@@ -1,12 +1,27 @@
 #include <lfb.h>
 #include <stdio.h>
+#include <string.h>
 
-extern void sd_test();
+#include <sd.h>
+#include <uart0.h>
 
+void sd_test()
+{
+	unsigned char buffer[512];
+	sd_init();
+	sd_readblock(614400,  buffer, 1);
+	buffer[17] = 0;
+	puts((char*) buffer);
+
+	char output[] = "RASPBERRY PI 3 WRITEBACK";
+	strcpy(buffer, output);
+	sd_writeblock(buffer, 614400, 1);
+
+}
 void kernel_main()
 {
-	lfb_init_as_stdout();
-
+	//lfb_init_as_stdout();
+	uart0_init_as_stdio();
 	sd_test();
 
 	puts("That's me done");
