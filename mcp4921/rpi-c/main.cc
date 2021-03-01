daa07eb80d477116e44245a8ad98c1238b2cd471
Mark Carter <alt.mcarter@gmail.com>
2021-03-01 23:16:48 +0000

    completed mcp4921 for Pi

--- mcp4921/rpi-c/main.cc
+++ mcp4921/rpi-c/main.cc
@@ -16,20 +16,12 @@ std::string slurp(const char *filename)
 }
 
 void dac_write(uint16_t value) {
-	//value &= 0b0000111111111111;
 	if(value>4095) value=4095;
 	value |= 0b0011000000000000;
 	char buf[2];
 	buf[0] = value >>8;
 	buf[1] = value & 0xff;
 	bcm2835_spi_writenb(buf, 2);
-	/*
-	   SPI.beginTransaction(dac_spi_settings);
-	   digitalWrite(DAC_CS, LOW);
-	   SPI.transfer16(value);
-	   digitalWrite(DAC_CS, HIGH);
-	   SPI.endTransaction();
-	   */
 }
 
 int main()
@@ -41,15 +33,16 @@ int main()
 	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
 	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
 	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_64);    // ~ 4 MHz
-	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_1024);    
+	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_16);    
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_1024);    
 	bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
 	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
 
-	int i = 0;
+	//int i = 0;
 	for(unsigned char c : song) {
-		//dac_write(c);
-		dac_write(i);
-		i = 1 - i;
+		dac_write((uint16_t) c << 4);
+		//dac_write(i);
+		//i = 1 - i;
 		bcm2835_delayMicroseconds(125);
 	}
 
