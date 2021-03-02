e4a82e80479058043a283cdfd40b053686c096c1
Mark Carter <alt.mcarter@gmail.com>
2021-03-02 19:56:34 +0000

    Added project rpi-sine440

---
+++ mcp4921/rpi-sine440/README.md
@@ -0,0 +1,25 @@
+# pi c
+
+
+## Connection
+
+```
+MCP INFO PI
+1   3V3   1
+2   CS   24
+3   SCK  23
+4   MOSI 18
+5   GND   6
+6   3V3   1
+7   GND   6
+8   VOUT  -
+
+COLS:
+* MCP - pin connection to MCP4921
+* INFO - description. VOUT goes to amp, not Pi
+* PI - physical pin for Pi connection
+```
+
+## NEWS
+
+2021-03-02 Started.  
---
+++ mcp4921/rpi-sine440/main.cc
@@ -0,0 +1,36 @@
+//#include <fstream>
+//#include <sstream>
+#include <iostream>
+#include <string>
+
+#include <bcm2835.h>
+
+#include "mcp4921.h"
+
+using namespace std;
+
+mcp4921 mcp;
+
+
+int main()
+{
+
+	string line;
+
+	while(1) {
+		printf("Enter level: 0-4095: ");
+		try {
+			getline(cin, line);
+			int level = stoi(line);
+			if(level<0 || level > 4095) throw std::invalid_argument("Out of bounds");
+			cout << "Theoretical voltage: " << (float)level *3.3/4096 << "\n";
+			mcp.write(level);
+			bcm2835_delayMicroseconds(125);
+		} catch (const std::invalid_argument& e) {
+			cerr << "Invalid argument. " << e.what() << "\n";
+		}
+	}
+
+	return 0;
+}
+
