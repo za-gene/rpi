3e98a6a088a3a26a8e3d1dfaa6e2267712990748
Mark Carter <alt.mcarter@gmail.com>
2021-03-03 13:18:46 +0000

    dpc enahncements - not working yet, though.

--- pico/dpc/main.cc
+++ pico/dpc/main.cc
@@ -3,6 +3,8 @@
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
 #include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
 
 
 #define	PIN_SCK		2
@@ -12,6 +14,40 @@
 
 #define BTN 14 // GPIO number, not physical pin
 #define LED 25 // GPIO of built-in LED
+
+class pwm {
+	public:
+		pwm();
+		void set_level(uint16_t vol);
+	private:
+		uint _slice_num;
+};
+
+pwm::pwm()
+{
+	gpio_set_function(0, GPIO_FUNC_PWM); // Tell GPIO 0 it is allocated to the PWM
+	_slice_num = pwm_gpio_to_slice_num(0); // get PWM slice for GPIO 0 (it's slice
+
+
+	// run the clock at 44.1kHz
+	uint32_t f_sys = clock_get_hz(clk_sys); // typically 125'000'000 Hz
+	float divider = f_sys / 8'000UL;  
+	pwm_set_clkdiv(_slice_num, divider); // pwm clock should now be running at 1MHz
+
+	pwm_set_wrap(_slice_num, 4095);
+	set_level(0) ;
+	pwm_set_enabled(_slice_num, true); // let's go!
+
+}
+
+void pwm::set_level(uint16_t vol)
+{
+	pwm_set_chan_level(_slice_num, 0, vol); 
+}
+
+
+
+pwm pwm0;
 
 int main() 
 {
@@ -30,14 +66,15 @@ int main()
 	for(;;) {
 		uint16_t rx, tx = 42;
 		spi_read16_blocking(spi0, tx, &rx, 1);
+		rx &= 0xfff; // 12-bit value
 		printf("Received %d\n", rx);
+		pwm0.set_level(rx);
 	}
 
-
+	/*
 	gpio_init(BTN);
 	gpio_set_dir(BTN, GPIO_IN);
 	gpio_pull_up(BTN);
-	// gpio_get() gets state of pin
 
 	gpio_init(LED);
 	gpio_set_dir(LED, GPIO_OUT);
@@ -48,6 +85,7 @@ int main()
 		gpio_put(LED, 0);
 		sleep_ms(1000);
 	}
+	*/
 
 	return 0;
 }
