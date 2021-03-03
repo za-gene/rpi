c280a1d168d8b2715a49e3c355f474b732d6d54a
Mark Carter <alt.mcarter@gmail.com>
2021-03-03 18:38:34 +0000

    abstracted blinkt

--- pico/blinkt/main.cc
+++ pico/blinkt/main.cc
@@ -4,13 +4,14 @@
 //#include "hardware/spi.h"
 #include "hardware/gpio.h"
 
+#include "blinkt.h"
 
 #define dataPin  16
 #define clockPin 17
 
 typedef uint8_t u8;
 
-
+#if 0
 const int numLEDs = 8;
 uint8_t pixels[numLEDs * 3];
 
@@ -32,13 +33,14 @@ void digitalWrite(u8 pin, u8 value)
 	else
 		gpio_put(pin, 1);
 }
+#endif
 
 void delay(int ms)
 {
 	sleep_ms(ms);
 }
 
-
+#if 0
 void spi_out(uint8_t n) {
 	for (uint8_t i = 8; i--; n <<= 1) {
 		if (n & 0x80) digitalWrite(dataPin, HIGH);
@@ -64,6 +66,7 @@ void show() {
 	for (i = 0; i < ((numLEDs + 15) / 16); i++) spi_out(0xFF); // end-frame marker
 }
 
+
 void setPixelColour(uint8_t pos , uint8_t r, uint8_t g, uint8_t b) {
 
 	pos *= 3;
@@ -71,15 +74,17 @@ void setPixelColour(uint8_t pos , uint8_t r, uint8_t g, uint8_t b) {
 	pixels[pos++] = g;
 	pixels[pos] = r;
 }
-
+#endif
 
 void loop() {
 	static uint8_t red = 0;
 	red = 10 - red; // switch between on and off
 	for (int i = 0; i < 8; ++i) {
-		show();
+		blinkt_show();
 		delay(100);
-		setPixelColour(i, red, 0, 0);
+		//blinkt_set_pixel_colour(i, 0, red, 0);
+		//setPixelColour(i, 0, red, 0);
+		blinkt_set_pixel_colour(i, red, 0, 0);
 	}
 
 }
@@ -89,13 +94,14 @@ void loop() {
 int main() 
 {
 	stdio_init_all();
-
+	blinkt_init(dataPin, clockPin);
+/*
 	pinMode(dataPin , OUTPUT);
 	pinMode(clockPin, OUTPUT);
 	digitalWrite(dataPin , LOW);
 	digitalWrite(clockPin, LOW);
 	show();
-
+*/
 	for(;;) loop();
 	return 0;
 }
