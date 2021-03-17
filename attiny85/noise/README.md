60e0695695c7f25ecaaa3d13e5450e066f78b719
Mark Carter <alt.mcarter@gmail.com>
2021-03-17 20:35:24 +0000

    t85 noise

--- attiny85/noise/README.md
+++ attiny85/noise/README.md
@@ -1,3 +1,6 @@
 # noise
 
 Produce noise on PB1 (pin 6).
+
+I suspect that the random number generator isn't fast enough, which 
+interferes with the production of white noise
--- attiny85/noise/main.cc
+++ attiny85/noise/main.cc
@@ -2,17 +2,21 @@
 
 #include <avr/io.h>
 #include <avr/iotnx5.h> // for CLKPR
-#include <util/delay.h>
+//#include <util/delay.h>
 #include <stdlib.h>
+//#include <avr/power.h>
 
 
 #define NOISE_PIN (1<<PB1)
 
 int main()
 {
+	// set clock to 8Mhz
+	CLKPR = 1 << CLKPCE; // Tell chip we want to scale the clock
+	CLKPR = 0; // prescaler is 1, giving 8MHz
+
 	DDRB |= NOISE_PIN; // for output
 	for(;;) {
-		continue;
 		if((random() % 2) == 0)
 			PORTB |= NOISE_PIN;
 		else
