0f36bcaab836b51a65a49c1cd5761fb935f71492
Mark Carter <alt.mcarter@gmail.com>
2021-03-31 13:01:09 +0100

    freq-divider tweaks
    For some reason even a basic LED isn't displaying

--- pico/freq-divider/.gdbinit
+++ pico/freq-divider/.gdbinit
@@ -3,5 +3,7 @@ file app.elf
 target remote localhost:3333
 load
 monitor reset init
+b main:32
+#b main.cc:50
 echo Type c to continue...\n
 
--- pico/freq-divider/main.cc
+++ pico/freq-divider/main.cc
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-#include "pico/stdlib.h"
+//#include "pico/stdlib.h"
 //#include "hardware/spi.h"
 #include "hardware/gpio.h"
 
 
 //#define BTN 14 // GPIO number, not physical pin
-//#define LED 25 // GPIO of built-in LED
+#define LED 25 // GPIO of built-in LED
 
-void freq_input_rise_isr(unit gpio, uint32_t events)
+void freq_input_rise_isr(uint gpio, uint32_t events)
 {
 	static uint32_t cnt = 0;
 	uint32_t factor = 1;
 	for(int i = 1; i < 11; i++) {
 		factor <<= 1;
 		//flip(i, factor);
-		gpio_out(i, (cnt % factor) >= (factor/2));
+		gpio_put(i, (cnt % factor) >= (factor/2));
 	}
 	cnt++;
 }
@@ -23,13 +23,16 @@ void freq_input_rise_isr(unit gpio, uint32_t events)
 
 int main() 
 {
-	stdio_init_all();
+	//stdio_init_all();
 
+#if 0
 	for(int i = 1; i < 11; i++) {
 		gpio_set_dir(i, GPIO_OUT);
 	}
-
-	gpio_set_irq_enabled_with_callback(0, GPIO_IRQ_EDGE_RISE, true, freq_input_rise_isr);
+#endif
+	gpio_set_dir(LED, GPIO_OUT);
+	gpio_put(LED, true);
+	//gpio_set_irq_enabled_with_callback(0, GPIO_IRQ_EDGE_RISE, true, &freq_input_rise_isr);
 	//gpio_init(BTN);
 	//gpio_set_dir(BTN, GPIO_IN);
 	//gpio_pull_up(BTN);
@@ -47,7 +50,8 @@ int main()
 	}
 	*/
 
-	for(;;);
+	//gpio_put(1, 1);
+	while(1);
 
 	return 0;
 }
