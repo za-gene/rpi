6d7c7aa8ec4af1ba348f4dabdb68aef6790ff600
Mark Carter <alt.mcarter@gmail.com>
2021-03-31 14:29:28 +0100

    freq divisor update

--- pico/freq-divider/README.md
+++ pico/freq-divider/README.md
@@ -1,7 +1,28 @@
-# ?
+# Frequency Divider
+
+It divides the input from GPIO0 on rising edge.
+
+The amount of division is dependent upon the pin:
+
+```
+GPIO DIVISOR
+1          2
+2          4
+3          8
+
+4         16
+5         32
+6         64
+
+7        128
+8        256
+9        512
+
+10      1024
+```
 
 ## News
 
-2021-03-30 Started
+2021-03-30 Started. Working
 
 
--- pico/freq-divider/main.cc
+++ pico/freq-divider/main.cc
@@ -8,7 +8,6 @@
 //#define BTN 14 // GPIO number, not physical pin
 //#define LED 25 // GPIO of built-in LED
 
-#if 0
 void freq_input_rise_isr(uint gpio, uint32_t events)
 {
 	static uint32_t cnt = 0;
@@ -20,23 +19,20 @@ void freq_input_rise_isr(uint gpio, uint32_t events)
 	}
 	cnt++;
 }
-#endif
 
 int main() 
 {
 	//stdio_init_all();
 
-#if 0
 	for(int i = 1; i < 11; i++) {
 		gpio_init(i);
 		gpio_set_dir(i, GPIO_OUT);
 	}
-#endif
-	const uint LED = PICO_DEFAULT_LED_PIN;
-	gpio_init(LED);
-	gpio_set_dir(LED, GPIO_OUT);
-	gpio_put(LED, 1);
-	//gpio_set_irq_enabled_with_callback(0, GPIO_IRQ_EDGE_RISE, true, &freq_input_rise_isr);
+
+
+	gpio_init(0);
+	//gpio_set_dir(0, GPIO_IN);
+	gpio_set_irq_enabled_with_callback(0, GPIO_IRQ_EDGE_RISE, true, &freq_input_rise_isr);
 	//gpio_init(BTN);
 	//gpio_set_dir(BTN, GPIO_IN);
 	//gpio_pull_up(BTN);
@@ -57,6 +53,6 @@ int main()
 	//gpio_put(1, 1);
 	while(1) sleep_ms(250);
 
-	//return 0;
+	return 0;
 }
 
