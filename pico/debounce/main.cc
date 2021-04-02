61162d12cf1f90aaddb1ecbb01b8a8aa34041bcc
Mark Carter <alt.mcarter@gmail.com>
2021-04-02 10:32:27 +0100

    added pico/debounce

---
+++ pico/debounce/main.cc
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+//#include "hardware/spi.h"
+#include "hardware/gpio.h"
+
+
+#define BTN 14 // GPIO number, not physical pin
+#define LED 25 // GPIO of built-in LED
+
+int main() 
+{
+	stdio_init_all();
+
+	gpio_init(BTN);
+	gpio_set_dir(BTN, GPIO_IN);
+	gpio_pull_up(BTN);
+	// gpio_get() gets state of pin
+
+	gpio_init(LED);
+	gpio_set_dir(LED, GPIO_OUT);
+
+	for(;;) {
+		gpio_put(LED, 1);
+		sleep_ms(100);
+		gpio_put(LED, 0);
+		sleep_ms(1000);
+	}
+
+	return 0;
+}
+
