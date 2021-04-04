9a1b46ec21f9505cbe12e92a8ec1ba6fbe26c15a
Mark Carter <alt.mcarter@gmail.com>
2021-04-04 19:08:50 +0100

    0TEMPLATE update to USB serial

--- pico/0TEMPLATE/main.cc
+++ pico/0TEMPLATE/main.cc
@@ -20,11 +20,13 @@ int main()
 	gpio_init(LED);
 	gpio_set_dir(LED, GPIO_OUT);
 
+	int i = 0;
 	for(;;) {
+		printf("Hello number %d\n", i++);
 		gpio_put(LED, 1);
 		sleep_ms(100);
 		gpio_put(LED, 0);
-		sleep_ms(1000);
+		sleep_ms(1000);		
 	}
 
 	return 0;
