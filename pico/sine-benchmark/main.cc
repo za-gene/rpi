6c4b2a0ab31fa13750253240dc781f7f35884980
Mark Carter <alt.mcarter@gmail.com>
2021-04-09 14:50:32 +0100

    new dir:   sine-benchmark

---
+++ pico/sine-benchmark/main.cc
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+//#include "hardware/adc.h"
+#include "hardware/gpio.h"
+//#include "hardware/spi.h"
+
+
+#define BTN 14 // GPIO number, not physical pin
+#define LED 25 // GPIO of built-in LED
+
+int main() 
+{
+	stdio_init_all();
+
+	gpio_init(BTN);
+	gpio_set_dir(BTN, GPIO_IN);
+	gpio_pull_up(BTN);
+	// gpio_get() gets state of pin
+
+	gpio_init(LED);
+	gpio_set_dir(LED, GPIO_OUT);
+
+	int i = 0;
+	for(;;) {
+		printf("Hello number %d\n", i++);
+		gpio_put(LED, 1);
+		sleep_ms(100);
+		gpio_put(LED, 0);
+		sleep_ms(1000);		
+	}
+
+	return 0;
+}
+
