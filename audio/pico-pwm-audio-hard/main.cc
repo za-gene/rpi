b334fd16dc412edff87cc5da8f08172960a9673a
Mark Carter <alt.mcarter@gmail.com>
2021-04-16 16:12:39 +0100

    tweaked pico harded pwm

--- audio/pico-pwm-audio-hard/main.cc
+++ audio/pico-pwm-audio-hard/main.cc
@@ -9,7 +9,8 @@
 #include "hardware/clocks.h"
 #include "hardware/irq.h"
 
-#include "track.h"
+#include "pace.h"
+#include "../mcp4921/pico-hard-song-mcp4921/data.h"
 
 #define	PIN_SCK		2
 #define	PIN_MOSI	3
@@ -20,106 +21,46 @@ using u16 = uint16_t;
 
 void my_pwm_wrap_isr();
 
-class digiout {
-	public:
-		digiout(uint8_t pin);
-		void put(bool value);
-		void toggle();
-	private:
-		bool _on = false;
-		uint8_t _pin;
-};
 
-digiout::digiout(uint8_t pin)
-{
-	_pin = pin;
-	gpio_init(pin);
-	gpio_set_dir(pin, GPIO_OUT);
-}
+#define LED 25
+#define SPK 19
 
-void digiout::put(bool value)
-{
-	_on = value !=0;
-	gpio_put(_pin, _on);
-}
-
-void digiout::toggle()
-{
-	put(!_on);
-}
-
-class pwm {
-	public:
-		pwm();
-		void set_level(uint16_t vol);
-	private:
-		uint _slice_num;
-};
-
-//digiout pin16(16);
-//digiout pin17(17);
-
-
-
+unsigned int slice_num;
+const int sampling_freq = 44100;
 const int top = 1023;
-const int f_pwm = 32000;
-pwm::pwm()
-{
-	// the PWM channel number is documents in the datasheet, s4.5.2 p535
-	// GPIO14 is 7A, 15 is 7B
-	gpio_set_function(14, GPIO_FUNC_PWM); 
-	gpio_set_function(15, GPIO_FUNC_PWM); 
-	_slice_num = pwm_gpio_to_slice_num(14); 
-	// assert that they're both on slice 7, as expected
-	bool same_slice = _slice_num == 7 && pwm_gpio_to_slice_num(15) == 7;
-	//pin17.put(same_slice);
-
-
-	// run the clock at 44.1kHz
-	uint32_t f_sys = clock_get_hz(clk_sys); // typically 125'000'000 Hz
-	float scale = (top+1) * f_pwm;
-	float divider = f_sys / scale;  
-	//divider = 1.0;
-	pwm_set_clkdiv(_slice_num, divider); // pwm clock should now be running at 1MHz
-
-	pwm_set_wrap(_slice_num, top);
-	set_level(2000) ;
-	pwm_set_enabled(_slice_num, true); // let's go!
-	pwm_set_chan_level(_slice_num, PWM_CHAN_B, 200); // GPIO15 is a trigger
-
-	pwm_clear_irq(_slice_num);
-	pwm_set_irq_enabled(_slice_num, true);
-	//irq_add_shared_handler(PWM_IRQ_WRAP, my_pwm_wrap_isr, PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
-	irq_set_exclusive_handler(PWM_IRQ_WRAP, my_pwm_wrap_isr);
-	irq_set_enabled(PWM_IRQ_WRAP, true);
-}
-
-void pwm::set_level(uint16_t vol)
-{
-	pwm_set_chan_level(_slice_num, PWM_CHAN_A, vol); 
-	//pwm_set_enabled(_slice_num, true); // let's go!
-}
-
-
-
-pwm a_pwm;
+//const int f_pwm = 32000;
 
 void my_pwm_wrap_isr()
 {
-	const int time_scale = f_pwm / 8000;
+	//const int time_scale = f_pwm / 8000;
 	const int vol_scale = (top+1)/256;
 	static int i = 0;
-	u16 v = track_raw[i/time_scale];
+	//u16 v = data_bin[i/time_scale];
+	u16 v = data_bin[i];
 	i++;
-	if(i == time_scale * sizeof(track_raw)) i = 0;
-	a_pwm.set_level(v*vol_scale);
+	if(i == data_bin_len) i = 0;
+	//a_pwm.set_level(v*vol_scale);
+	int new_level = v*vol_scale;
+	pwm_set_gpio_level(SPK, new_level);
 
 	//pin16.toggle();
-	pwm_clear_irq(7); // pwm gpio 14 and 15 on slice 7
+	pwm_clear_irq(slice_num); 
 }
 
 int main() 
 {
+	stdio_init_all();
+	int err = pace_config_pwm_irq(&slice_num, SPK, sampling_freq, top, my_pwm_wrap_isr);
+        if(err) {
+            puts("Can't set PWM");
+            for(;;); // loop forever, effectively halting
+        }
+
+
+	gpio_init(LED);
+	gpio_set_dir(LED, GPIO_OUT);
+	gpio_put(LED, 1);
+
 	while(1); // actual playing of track is handled by my_pwm_wrap_isr()
 
 	return 0;
