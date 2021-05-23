85e9d1ed2f3cddcb6c9a0f0b797b99ca08aa27a4
Mark Carter <alt.mcarter@gmail.com>
2021-05-23 15:40:11 +0100

    sdcard pico tweaks
    just added oled stuff

--- pico/sdcard/main.cc
+++ pico/sdcard/main.cc
@@ -7,15 +7,44 @@
 #include "hardware/gpio.h"
 //#include "hardware/irq.h"
 //#include "hardware/pwm.h"
-//#include "hardware/spi.h"
+#include "hardware/spi.h"
 // #include "tusb.h" // if you want to use tud_cdc_connected()
 
+#include "../../1306/pico-sdk/oled.h"
 
+#define	PIN_SCK		10
+#define	PIN_MOSI	11
+#define PIN_MISO 	12
+#define	PIN_CS 		15
+
+void init_card()
+{
+	// standard spi stuff
+	int spi_speed = 1'200'000;
+	spi_speed = 600'000;
+	spi_init(spi1, spi_speed);
+	//spi_set_slave(spi0, true);
+	//spi_set_format(spi0, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+	gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+	//gpio_set_function(PIN_CS,   GPIO_FUNC_SPI);
+	gpio_set_dir(PIN_CS, GPIO_OUT);
+	gpio_put(PIN_CS, 1);
+	gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
+	gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+
+}
 
 int main() 
 {
 	stdio_init_all();
 	// while(!tud_cdc_connected()) sleep_ms(250); // wait for usb serial 
+
+	init_display(64, 6);
+	//init_spi();
+	init_card();
+	ssd1306_print("1");
+	show_scr();
+
 
 #define BTN  14 // GPIO number, not physical pin
 #define LED  25 // GPIO of built-in LED
