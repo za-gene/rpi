667f2f3b520ab2f9d1db7ed4ba20bb245c43610b
Mark Carter <alt.mcarter@gmail.com>
2021-05-24 15:25:29 +0100

    sd card init
    We now seem to be able to initialise an SD card for the Pico.
    Many shortcuts were taken.

--- pico/sdcard/main.cc
+++ pico/sdcard/main.cc
@@ -26,7 +26,10 @@
 #define SDTOUT -2 // timeout waiting for card to be ready
 #define SDROUT -3 // timeout waiting for card to respond to command
 #define SDCMD0 -4 // failed CMD0 (card couldn't be set to idle state)
-#define SDCMD8 -5 // failed CMD8 (identify card version (only version 1 is handled))
+#define SDCMD8 -5 // failed CMD8 (identify card version (only version 2 is handled))
+#define SDFV2  -6 // failed to initialise card as version 2 type card
+#define SDCMD16 -7 // CMD16 failed to set card block size to 512
+
 
 void cs_low() {	gpio_put(PIN_CS, 0); }
 
@@ -34,6 +37,9 @@ void cs_high() { gpio_put(PIN_CS, 1); }
 
 typedef uint8_t u8;
 
+int cdv = 512; // card is byte addressing, set to 1 if addresses are per block
+
+//u8 ocr[4]; // 
 #if 0
 void simple_write_read(uint8_t* src, uint8_t* dst, int len)
 {
@@ -97,12 +103,8 @@ Trans::~Trans()
 	spi_write_blocking(spi, &b, 1); // just spin our wheels so that the card can complete its operation
 }
 
-int CMD0(int cmd, int arg, int crc)
+void call_cmd(int cmd, int arg, int crc)
 {
-	Trans t;
-	//if(wait_for_ready()) return SDTOUT;
-	wait_for_ready();
-
 	uint8_t buf[6];
 	buf[0] = 0x40 | cmd;
 	buf[1] = (arg >> 24) & 0xFF;
@@ -113,6 +115,15 @@ int CMD0(int cmd, int arg, int crc)
 
 	//simple_write(buf, sizeof(buf));
 	spi_write_blocking(spi, buf, sizeof(buf));
+}
+
+int CMD_T1(int cmd, int arg, int crc)
+{
+	Trans t;
+	//if(wait_for_ready()) return SDTOUT;
+	wait_for_ready();
+	call_cmd(cmd, arg, crc);
+
 
 	//if(skip1)  read_byte(0xFF);
 
@@ -133,26 +144,14 @@ int CMD0(int cmd, int arg, int crc)
 	return SDROUT;
 }
 
-int CMD8(int cmd, int arg, int crc)
+int CMD_T2(int cmd, int arg, int crc, u8 quad[4])
 {
 	Trans t;
 	//if(wait_for_ready()) return SDTOUT;
 	int status = wait_for_ready();
 	if(status) return SDTOUT;
 
-	uint8_t buf[6];
-	buf[0] = 0x40 | cmd;
-	buf[1] = (arg >> 24) & 0xFF;
-	buf[2] = (arg >> 16) & 0xFF;
-	buf[3] = (arg >> 8) & 0xFF;
-	buf[4] = (arg >> 0) & 0xFF;
-	buf[5] = crc;
-
-	//simple_write(buf, sizeof(buf));
-	spi_write_blocking(spi, buf, sizeof(buf));
-
-
-	//if(skip1)  read_byte(0xFF);
+	call_cmd(cmd, arg, crc);
 
 	// wait for response[7] == 0
 	uint8_t resp;
@@ -162,9 +161,9 @@ int CMD8(int cmd, int arg, int crc)
 		//if(buf[0] & 0x80) continue;
 
 		if(!(resp & 0x80)) {
-			uint8_t resp_buf[4];
+			//uint8_t resp_buf[4];
 			//printf("Got a response from CMD8");
-			spi_read_blocking(spi, 0xFF, resp_buf, sizeof(resp_buf));
+			spi_read_blocking(spi, 0xFF, quad, 4);
 			return resp;
 		}
 
@@ -172,6 +171,25 @@ int CMD8(int cmd, int arg, int crc)
 	}
 
 	return SDCMD8;
+}
+
+int init_card_v2()
+{
+	u8 ocr[4];
+	for(int i=0; i< CMD_TIMEOUT; i++) {
+		printf("init_card_v2 try %d\n", i);
+		sleep_ms(50);
+		CMD_T2(58, 0, 0xFD, ocr);
+		CMD_T1(55, 0, 0x65);
+		constexpr int arg = 0x40000000;
+		static_assert((arg>0) && (sizeof(int)>=4));
+		if(CMD_T1(41, arg, 0x77) == 0) {
+			CMD_T2(58, 0, 0xFD, ocr);
+			if((ocr[0] & 0x40) != 0) cdv = 1;
+			return 0;
+		}
+	}
+	return SDFV2;
 }
 
 int init_card()
@@ -204,7 +222,7 @@ int init_card()
 	int status;
 	for(int i = 0; i < 10; i++) {
 		printf("\nidle attempt %d\n", i);
-		status = CMD0(0, 0, 0x95);
+		status = CMD_T1(0, 0, 0x95);
 		printf("status=%d\n", status);
 		if(status == R1_IDLE_STATE) {
 			//ssd1306_print(" IDLE ");
@@ -219,19 +237,25 @@ int init_card()
 	}
 
 	// CMD8: card version
-	// We oncly consider version 1 cards at this point
-	status = CMD8(8, 0x01aa, 0x87);
+	// We only consider version 2 cards at this point
+	u8 cmd8_response[4];
+	status = CMD_T2(8, 0x01aa, 0x87, cmd8_response);
 	printf("\nCMD8 card status %d\n", status);
 	if(status != R1_IDLE_STATE) 
 		return SDCMD8;
 
-	/*
-	if(status == R1_ILLEGAL_COMMAND) {
-		printf("version 1 or not sd card\n");
-	} else {
-		printf("version 2 or later card\n");
-	}
-	*/
+	// we are assuming version 2 cards
+	printf("Attempting init_card_v2()\n");	
+	status = init_card_v2();
+	if(status) return status;
+
+	// skip CMD9 that adafruit uses
+	
+
+	// CMD16 set block length to 512 bytes
+	if(CMD_T1(16, 512, 0x15) != 0) return SDCMD16;
+	printf("CMD16 set block size to 512 successfully\n");
+
 
 	return 0;
 
