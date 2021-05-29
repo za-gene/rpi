7260484ee8d9c0cdc6ebcf8718ecaa117f90332f
Mark Carter <alt.mcarter@gmail.com>
2021-05-29 17:19:15 +0100

    new file:   ../picomc.cmake

---
+++ pico/picomc.cmake
@@ -0,0 +1,20 @@
+message("picomc.cmake says hi")
+
+
+#project(app C CXX ASM) # needs to be in the projects file, I think
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
+
+
+#set(PICO_CXX_ENABLE_EXCEPTIONS 1)
+
+include($ENV{PICO_SDK_PATH}/pico_sdk_init.cmake)
+include_directories(..)
+#pico_sdk_init()
+
+add_custom_target(flash
+    COMMAND cp app.uf2 /media/pi/RPI-RP2
+    DEPENDS app
+)
+
+
