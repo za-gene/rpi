0cd3ffd3e78c7a320f2b12a3bd575f7186beb70b
Mark Carter <alt.mcarter@gmail.com>
2021-06-08 17:22:07 +0100

    A semi-working timer-irq

--- stm32f411re/libopencm3/timer-irq/main.c
+++ stm32f411re/libopencm3/timer-irq/main.c
@@ -1,26 +1,66 @@
+#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
-#include <libopencm3/stm32/spi.h>
+//#include <libopencm3/stm32/spi.h>
 
 typedef uint32_t u32;
 
+#define GPIOn GPIO14
+
+volatile int on =0;
+void tim2_isr(void) // the standard ISR name for TIM2
+{
+#if 1
+	if(on) 
+		gpio_set(GPIOC, GPIOn);
+	else
+		gpio_clear(GPIOC, GPIOn);
+
+	on = 1-on;
+#else
+	gpio_toggle(GPIOC, GPIOn);
+#endif
+	//TIM_CNT(TIM2) = 1;
+	timer_clear_flag(TIM2, TIM_SR_UIF); // clear the interrupt flag so that it can retrigger
+}
 
 int main(void)
 {
+	// Set up the builtin LED on PC13
 	rcc_periph_clock_enable(RCC_GPIOC);
-	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
-	gpio_set(GPIOC, GPIO13);
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIOn);
+	//gpio_set(GPIOC, GPIO13);
 
+	// trigger the isr twice a second
+	rcc_periph_clock_enable(RCC_TIM2);
+	volatile u32 clk_freq = rcc_get_timer_clk_freq(TIM2);
+#if 1
+	timer_set_prescaler(TIM2, clk_freq/1000000-1); // set the CK_CNT (clock counter) freq to 1MHz
+	double freq = 100.0; //hz
+	volatile double period = 1000000.0/freq -1.0;
+	timer_set_period(TIM2, period); // twice a second
+#else
+	timer_set_prescaler(TIM2, 800000);
+	timer_set_period(TIM2, 40000);
+#endif
+	/* Set timer start value. */
+	TIM_CNT(TIM2) = 1;
+	//timer_disable_preload(TIM2); //fishy
+	//timer_continuous_mode(TIM2); //fishy
+	timer_generate_event(TIM2, TIM_EGR_UG);
+	timer_generate_event(TIM2, TIM_EGR_TG | TIM_EGR_UG);
+	timer_enable_counter(TIM2); // seems unimportant
+	timer_enable_irq(TIM2, TIM_DIER_UIE); // update interrupt enable
+	nvic_enable_irq(NVIC_TIM2_IRQ);
 
-#define LITTLE_BIT 1000000
-	while (1)
-	{
-		/* wait a little bit */
-		for (int i = 0; i < LITTLE_BIT; i++)
-		{
-			__asm__("nop");
-		}
-		gpio_toggle(GPIOC, GPIO13);
-	}
+	/* Update interrupt enable. */
+	TIM_DIER(TIM2) |= TIM_DIER_UIE;
+
+	/* Start timer. */
+	TIM_CR1(TIM2) |= TIM_CR1_CEN;
+	//timer_enable_counter(TIM2);
+	//rcc_periph_reset_pulse(RST_TIM2);
+
+	while(1);
 }
