1f1e1253a53264db5dd18b96e068e173bfb46fe5
Mark Carter <alt.mcarter@gmail.com>
2021-06-11 15:45:18 +0100

    usart running

--- stm32f411re/libopencm3/usart/main.c
+++ stm32f411re/libopencm3/usart/main.c
@@ -2,6 +2,7 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
 #include <libopencm3/stm32/spi.h>
+#include <libopencm3/stm32/usart.h>
 
 typedef uint32_t u32;
 
@@ -12,6 +13,17 @@ int main(void)
 	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
 	gpio_set(GPIOC, GPIO13);
 
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_USART1);
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF,GPIO_PUPD_NONE, GPIO9); // TX
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO9);
+	usart_set_baudrate(USART1, 115200);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART2, USART_STOPBITS_1);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_mode(USART1, USART_MODE_TX);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+	usart_enable(USART1);
 
 #define LITTLE_BIT 1000000
 	while (1)
@@ -22,5 +34,6 @@ int main(void)
 			__asm__("nop");
 		}
 		gpio_toggle(GPIOC, GPIO13);
+		usart_send_blocking(USART1, '.');
 	}
 }
