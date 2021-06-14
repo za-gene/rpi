6035a21f5be9a483ba561131a845632d26068d5c
Mark Carter <alt.mcarter@gmail.com>
2021-06-14 20:39:46 +0100

    Added project zeroseg

---
+++ pico/zeroseg/main.cc
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+//#include "hardware/adc.h"
+//#include "hardware/clocks.h"
+//#include "hardware/flash.h"
+#include "hardware/gpio.h"
+//#include "hardware/irq.h"
+//#include "hardware/pwm.h"
+//#include "hardware/spi.h"
+// #include "tusb.h" // if you want to use tud_cdc_connected()
+
+#include "pi.h"
+
+#define BTN  14 // GPIO number, not physical pin
+#define LED  25 // GPIO of built-in LED
+
+
+void gpio_callback(uint gpio, uint32_t events)
+{
+	pi_gpio_toggle(LED);
+}
+
+int main() 
+{
+	stdio_init_all();
+	// while(!tud_cdc_connected()) sleep_ms(250); // wait for usb serial 
+
+	pi_gpio_init(LED, OUTPUT);
+	pi_gpio_init(BTN, INPUT); // have it as floating
+	gpio_set_irq_enabled_with_callback(BTN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+
+	while(1);
+
+	return 0;
+}
+
