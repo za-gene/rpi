b0f7bbeb065342f0b5cf0e0e1ed162235ad8150d
Mark Carter <alt.mcarter@gmail.com>
2021-06-26 11:54:52 +0100

    tweaks to debugging

--- pico/debugc/main.c
+++ pico/debugc/main.c
@@ -17,6 +17,19 @@ int main()
 	stdio_init_all();
 	// while(!tud_cdc_connected()) sleep_ms(250); // wait for usb serial 
 
+	// dump out the second-stage bootloader
+	const int wpl = 4; // words per line
+	const int nrows = 256/4/wpl; // number of rows to output
+	puts("\n.section .boot2, \"a\"");
+	for(int r = 0; r< nrows; r++) {
+		printf(".word ");
+		for(int c = 0; c<wpl; c++) {
+			printf("0x%8.8x", *(uint32_t*) (0x10000000 + (r*wpl+c)*4));
+			if(c<(wpl -1)) printf(", ");
+		}
+		puts("");
+	}
+
 #define BTN  14 // GPIO number, not physical pin
 #define LED  25 // GPIO of built-in LED
 	gpio_init(BTN);
@@ -29,7 +42,7 @@ int main()
 
 	int i = 0;
 	for(;;) {
-		printf("Hello number %d\n", i++);
+		//printf("Hello number %d\n", i++);
 		gpio_put(LED, 1);
 		sleep_ms(100);
 		gpio_put(LED, 0);
