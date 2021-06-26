24147e6cd30d472ff4c7ef9313a59fa838161c83
Mark Carter <alt.mcarter@gmail.com>
2021-06-26 18:57:33 +0100

    debugc: making LED more primitive

--- pico/debugc/main.c
+++ pico/debugc/main.c
@@ -10,7 +10,30 @@
 //#include "hardware/spi.h"
 // #include "tusb.h" // if you want to use tud_cdc_connected()
 
+#define REG(addr) *(volatile uint32_t*)(addr)
 
+
+//#define SIO_BASE 		0xd0000000 // see s2.3.1.7
+#define SIO_GPIO_OUT		REG(SIO_BASE+0x010) // GPIO output value
+#define SIO_GPIO_OUT_SET	REG(SIO_BASE+0x014) // GPIO output value set
+#define SIO_GPIO_OUT_CLR	REG(SIO_BASE+0x018) // GPIO output value clear
+#define SIO_GPIO_OE		REG(SIO_BASE+0x020) // GPIO output enable
+#define SIO_GPIO_OE_SET		REG(SIO_BASE+0x024) // GPIO output enable set
+#define SIO_GPIO_OE_CLR 	REG(SIO_BASE+0x028) // GPIO output enable clear
+
+//#define IO_BANK0_BASE 		0x40014000
+#define IO_BANK0_GPIO25_CTRL 	REG(IO_BANK0_BASE+0x0cc)
+
+//#define PADS_BANK0_BASE 	0x4001c000 // see s2.19.6.3. Pad control register
+#define PADS_BANK0_GPIO25	REG(PADS_BANK0_BASE+0x68)
+
+
+#define GPIO_FUNC_SIO	5
+
+
+
+#define BTN  14 // GPIO number, not physical pin
+#define LED  25 // GPIO of built-in LED
 
 int main() 
 {
@@ -30,8 +53,6 @@ int main()
 		puts("");
 	}
 
-#define BTN  14 // GPIO number, not physical pin
-#define LED  25 // GPIO of built-in LED
 	gpio_init(BTN);
 	gpio_set_dir(BTN, GPIO_IN);
 	gpio_pull_up(BTN);
@@ -43,9 +64,11 @@ int main()
 	int i = 0;
 	for(;;) {
 		//printf("Hello number %d\n", i++);
-		gpio_put(LED, 1);
+		SIO_GPIO_OUT_SET = 1ul << LED; // turn LED on. Works
+		//gpio_put(LED, 1);
 		sleep_ms(100);
-		gpio_put(LED, 0);
+		//gpio_put(LED, 0);
+		SIO_GPIO_OUT_CLR = 1ul << LED; // turn off the LED.Works.
 		sleep_ms(1000);		
 	}
 
