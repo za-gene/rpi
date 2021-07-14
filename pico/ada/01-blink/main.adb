f15d7318c30246a196cf2cab96d6ef367fd1d21d
Mark Carter <alt.mcarter@gmail.com>
2021-07-14 20:28:01 +0100

    ada blink

---
+++ pico/ada/01-blink/main.adb
@@ -0,0 +1,22 @@
+--
+--  Copyright 2021 (C) Jeremy Grosser
+--
+--  SPDX-License-Identifier: BSD-3-Clause
+--
+with RP.Device;
+with RP.Clock;
+with RP.GPIO;
+with Pico;
+
+procedure Main is
+begin
+   RP.Clock.Initialize (Pico.XOSC_Frequency);
+   Pico.LED.Configure (RP.GPIO.Output);
+   RP.Device.Timer.Enable;
+   loop
+      Pico.LED.Set;
+      RP.Device.Timer.Delay_Milliseconds (100);
+      Pico.LED.Clear;
+      RP.Device.Timer.Delay_Milliseconds (900);
+   end loop;
+end Main;
