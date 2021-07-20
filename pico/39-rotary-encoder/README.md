443dcb18e354aa8922a3e37c6d5e477f0b11b56b
Mark Carter <alt.mcarter@gmail.com>
2021-07-20 11:48:06 +0100

    fixed and enhanced rotary encoder

--- pico/39-rotary-encoder/README.md
+++ pico/39-rotary-encoder/README.md
@@ -2,9 +2,18 @@
 
 Uses interrupts
 
+Wire up your rotary encoder as follows:
+
+```
+SIA	21
+SIB	20
+SW	19
+```
 
 
 ## Status
+
+2021-07-29	Working. Added button support and corrected pin error.
 
 2021-07-19	Started
 
--- pico/39-rotary-encoder/main.cc
+++ pico/39-rotary-encoder/main.cc
@@ -18,48 +18,19 @@
 #define DELAY 1'000
 
 
-#define SIA 20
+#define SIA 	21
 //Debounce sia(SIA, 1);
-#define SIB 21
+#define SIB 	20
+#define SW	19 
 //Debounce sib(SIB, 1);
 //#define BTN  22 // GPIO number, not physical pin
 #define LED  25 // GPIO of built-in LED
 
 volatile uint32_t count = 0;
 
-// NB as of 2021-06-21 the GPIO parameter is ignored, and is enabled on any pin. According to SDK
 
 volatile uint32_t ev_num = 0;
 
-void sia_falling(uint gpio, uint32_t events)
-{
-	/*
-	   if(gpio != SIA) return;
-	   printf("Event %d:", ev_num++);
-	   if(events & GPIO_IRQ_EDGE_FALL) {
-	   puts("Falling");
-	   }
-	   if(events & GPIO_IRQ_EDGE_RISE) {
-	   puts("Rising");
-	   }
-	   return;
-	   */
-	if(gpio_get(SIA) !=0) {
-		count++;
-		printf("Changed to = %d\n", count);
-		pi_gpio_toggle(LED);
-	}
-
-	//if(!gpio_get(SIB) && (events == GPIO_IRQ_EDGE_RISE))
-	//	count--;
-}
-void sia_rising(uint gpio, uint32_t events)
-{
-	if(!gpio_get(SIB)) 
-		count--;
-	printf("Decreased to = %d\n", count);
-	pi_gpio_toggle(LED);
-}
 
 int gpio_is_high(uint gpio)
 {
@@ -71,13 +42,14 @@ static void millis_irq()
 {
 	pi_alarm_rearm(ALARM, DELAY);
 	volatile static uint64_t millis = 0;
-	volatile static uint8_t prev_grate = gpio_is_high(SIA) * 0xFF;
-	volatile static uint8_t cur_grate= prev_grate;
-	volatile static int prev_state = (gpio_is_high(SIA) <<1) | gpio_is_high(SIB);
+	//volatile static uint8_t prev_grate = gpio_is_high(SIA) * 0xFF;
+	//volatile static uint8_t cur_grate= prev_grate;
 	volatile static int i = 0;
 	volatile static int count = 0;
+	static auto state = [](){return (gpio_is_high(SIB) <<1) | gpio_is_high(SIA); };
+	volatile static int prev_state = state();
 	if(millis % 1 == 0) {
-		int cur_state = (gpio_is_high(SIA) <<1) | gpio_is_high(SIB);
+		int cur_state = state();
 		int prev_count = count;
 		if(cur_state != prev_state) {
 			//printf("cur_state %d (ms) = %d, prev = %d\n", i++, cur_state, prev_state);
@@ -92,28 +64,15 @@ static void millis_irq()
 				printf("count %d = %d\n", i++, count);
 			prev_state = cur_state;
 		}
-#if 0
-		cur_grate = ( cur_grate << 1 ) & gpio_is_high(SIA);
-		printf("grate = %d\n", cur_grate);
-		if(cur_grate == 0 && prev_grate != 0) {
-			puts("sia fell to 0");
-		}
-		if(cur_grate == 0xFF && prev_grate != 0xFF) {
-			puts("sia went high");
-		}
-		prev_grate = cur_grate;
-#endif
 	}
 
-
-	if(millis%1000==0) {
+	if(millis % 500 == 0) {
 		//puts("hi");
 		pi_gpio_toggle(LED);
 	}
 
 	millis++;
 
-	//printf("Alarm IRQ fired %d\n", i++);
 }
 
 int main() 
@@ -124,52 +83,13 @@ int main()
 	pi_gpio_init(SIA, INPUT);
 	pi_gpio_init(SIB, INPUT);
 	pi_gpio_init(LED, OUTPUT);
-	//pi_max7219_init();
-	//pi_max7219_show_count(count);
-
-	//pi_gpio_init(LED, OUTPUT);
-	//pi_gpio_init(BTN, INPUT_PULLUP);
-	//gpio_set_irq_enabled_with_callback(SIA, GPIO_IRQ_EDGE_FALL, true, &sia_falling);
-	//gpio_set_irq_enabled_with_callback(SIB, GPIO_IRQ_EDGE_RISE, true, &sia_falling);
-	//gpio_set_irq_enabled_with_callback(SIA, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &sia_falling);
-	//gpio_set_irq_enabled_with_callback(SIA, GPIO_IRQ_EDGE_RISE, true, &sia_rising);
-
 	pi_alarm_init(ALARM, millis_irq, DELAY);
 
-	int i = 0;
-	int count = 0;
+	Debounce sw(SW);
 	while(1) {
-#if 0
-		volatile static auto prev_state = (gpio_is_high(SIA) <<1) | gpio_is_high(SIB);
-		auto cur_state = (gpio_is_high(SIA) <<1) | gpio_is_high(SIB);
-		if(cur_state != prev_state) {
-			printf("cur_state %d = %d\n", i++, cur_state);
-			prev_state = cur_state;
+		if(sw.falling()) {
+			puts("Rotary button pushed");
 		}
-#endif
-		continue;
-		/*
-		if(sia.falling()) {
-			if(gpio_get(SIB)) {
-				count++;
-				//else
-				//	count--;
-
-				printf("ncreasing %d\n", count);
-			}
-		}
-
-		if(sib.falling()) {
-			if(gpio_get(SIA)) {
-				count--;
-				printf("decreasing %d\n", count);
-			}
-		}
-
-
-		//pi_max7219_show_count(count);
-		//busy_wait_us_32(100*1000);
-		*/
 	}
 
 	return 0;
