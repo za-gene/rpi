ea99b88422b1ceb20fc0a6dbdf94d5d3ed2cefdd
Mark Carter <alt.mcarter@gmail.com>
2021-07-20 15:14:29 +0100

    turned rotary into a class

--- pico/39-rotary-encoder/main.cc
+++ pico/39-rotary-encoder/main.cc
@@ -33,12 +33,64 @@ volatile uint32_t count = 0;
 volatile uint32_t ev_num = 0;
 
 
-int gpio_is_high(uint gpio)
+int pi_gpio_is_high(uint gpio)
 {
 	if(gpio_get(gpio) == 0) return 0;
 	return 1;
 }
 
+class Rotary {
+	public:
+		Rotary(uint sia, uint sib, uint sw);
+		bool sw_falling();
+		bool sw_rising();
+		int change();
+	private:
+		uint m_sia, m_sib;
+		Debounce m_sw;
+		Pulse m_pulse;
+		int m_prev_state = 0, m_cur_state = 0, m_count_change = 0;
+		int state();
+};
+
+Rotary::Rotary(uint sia, uint sib, uint sw) : m_sia(sia), m_sib(sib), m_sw(Debounce(sw))
+{
+	pi_gpio_init(sia, INPUT);
+	pi_gpio_init(sib, INPUT);
+
+}
+
+
+bool Rotary::sw_falling() { return m_sw.falling(); }
+bool Rotary::sw_rising() { return m_sw.rising(); }
+
+int Rotary::state()
+{
+	return (pi_gpio_is_high(m_sib) <<1) | pi_gpio_is_high(m_sia); 
+}
+
+int Rotary::change()
+{
+	if(m_pulse.expired()) {
+		m_cur_state = state();
+		if((m_prev_state == 2) && (m_cur_state == 3)) {
+			m_count_change++;
+			//puts("increase");
+		}
+		if((m_prev_state == 1) && (m_cur_state == 3)) {
+			m_count_change--;
+		}
+		//if(count != prev_count)
+		//	printf("count %d = %d\n", ++count_num, count);
+		m_prev_state = m_cur_state;
+	}
+	auto tmp = m_count_change;
+	m_count_change = 0;
+	return tmp;
+
+}
+
+#if 0
 static void millis_irq()
 {
 	pi_alarm_rearm(ALARM, DELAY);
@@ -76,25 +128,30 @@ static void millis_irq()
 	millis++;
 
 }
+#endif
 
 int main() 
 {
 	stdio_init_all();
 	// while(!tud_cdc_connected()) sleep_ms(250); // wait for usb serial 
 
-	pi_gpio_init(SIA, INPUT);
-	pi_gpio_init(SIB, INPUT);
+	Rotary rot(SIA, SIB, SW);
 	pi_gpio_init(LED, OUTPUT);
-	pi_alarm_init(ALARM, millis_irq, DELAY);
+	//pi_alarm_init(ALARM, millis_irq, DELAY);
 
 	Pulse pulse(500);
-	Debounce sw(SW);
+	int count = 0;
+	int count_num = 0;
 	while(1) {
-		if(sw.falling()) {
+		if(rot.sw_falling()) {
 			puts("Rotary button pushed");
 		}
 		if(pulse.expired())
 			pi_gpio_toggle(LED);
+		if(int chg = rot.change()) {
+			count += chg;
+			printf("count %d = %d\n", ++count_num, count);
+		}
 	}
 
 	return 0;
