80747c66794281ca13bbcdc83f93d3f73c129770
Mark Carter <alt.mcarter@gmail.com>
2021-07-23 11:42:27 +0100

    project 39 now working

---
+++ pico/39-serial/README.md
@@ -0,0 +1,9 @@
+# serial
+
+A program for the PC rather than the Pico. 
+It echos output from the Pico. 
+Useful if you want to programmatically interact with the Pico
+
+## Status
+
+2021-07-21	Started. Working.
--- pico/39-serial/main.cc
+++ pico/39-serial/main.cc
@@ -1,4 +1,4 @@
-#include <assert>
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
@@ -9,14 +9,30 @@
 int main()
 {
 
-	int fd1=open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
-	assert(fd1 != -1);
-	fcntl(fd1, F_SETFL,0);
+	//int fd1=open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);
+	int fd1=open("/dev/ttyACM0", O_RDWR | O_NOCTTY);
+	assert(fd1>=0);
+
+	struct termios tty;
+	if(tcgetattr(fd1, &tty) != 0) {
+		printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+	}
+	cfsetspeed(&tty, B115200);
+	//fcntl(fd1, F_SETFL,0);
+	cfmakeraw(&tty); // make it a raw tty, which fixes \n issues that I was having before
+
+	// Save tty settings, also checking for error
+	if (tcsetattr(fd1, TCSANOW, &tty) != 0) {
+		printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+		return 1;
+	}
 
 	while(1) {
-	char c;
-	int rd = read(fd1, &c, 1);
-	putchar(c);
+		char c;
+		int rd = read(fd1, &c, 1);
+		//if(rd>0) 
+		//	printf("%d \n", c);
+		putchar(c);
 	}
 
 	close(fd1);
