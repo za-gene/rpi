fe1b3c32e01f8ba4f66c36ab04071a475c9195ef
Mark Carter <alt.mcarter@gmail.com>
2021-07-27 15:11:32 +0100

    modified:   riff.cc

--- pico/41-uudecode/riff.cc
+++ pico/41-uudecode/riff.cc
@@ -18,7 +18,7 @@ constexpr auto riff = chid('R', 'I', 'F', 'F');
 constexpr auto wave = chid('W', 'A', 'V', 'E');
 constexpr auto fmt_ = chid('f', 'm', 't', ' ');
 //constexpr uint32_t data = chid("DATA");
-constexpr auto data = chid('D', 'A', 'T', 'A');
+constexpr auto data = chid('d', 'a', 't', 'a');
 constexpr auto list = chid('L', 'I', 'S', 'T');
 
 struct hdr {
@@ -36,9 +36,14 @@ struct fmt_info {
 } fmt_info;
 
 
+size_t file_pos = 0;
+
+size_t file_size;
+
 void read_hdr()
 {
 	fread(&hdr, 8, 1, fp);
+	file_pos += 8;
 }
 
 int main()
@@ -48,11 +53,13 @@ int main()
 
 	read_hdr();
 	assert(hdr.id == riff);
+	file_size = hdr.size;
 
 	read_hdr();
 	assert(hdr.id == wave);
 	assert(hdr.size == fmt_);
 	size_t n = fread(&fmt_info, sizeof(struct fmt_info), 1, fp);
+	file_pos += sizeof(struct fmt_info);
 	assert(n==1);	
 	cout << "len = " << fmt_info.len_fmt << "\n";
 	assert(fmt_info.len_fmt == 16);
@@ -60,20 +67,29 @@ int main()
 	cout << "#channs: " << fmt_info.num_channels << "\n";
 	cout << "sample rate: " << fmt_info.sample_rate << "\n";
 
-	read_hdr();
-	switch(hdr.id) {
-		case list:
-			cout << "found a list\n" ;
-			break;
-		case data:
-			cout << "found a data\n" ;
-			break;
-		default:
-			cout << "unknwon chucnk id\n";
+	while(ftell(fp) < file_size) {
+	//for(int i = 0; i < 2; i++) {
+		read_hdr();
+		switch(hdr.id) {
+			case list:
+				cout << "found a list\n" ;
+				cout << "skipping " << hdr.size << "\n" ;
+				fseek(fp, hdr.size, SEEK_CUR);
+				file_pos += hdr.size; // skip it
+				break;
+			case data:
+				cout << "found data\n" ;
+				fseek(fp, hdr.size, SEEK_CUR);
+				break;
+			default:
+				cout << "unknown chunk id: " << hdr.id << "\n";
+				fseek(fp, hdr.size, SEEK_CUR);
+		}
 	}
-	
 
 
+
+	cout << "Bye\n";
 	fclose(fp);
 
 
