d796ac61dcf5649c769160514c057b2ad7e9ce60
Mark Carter <alt.mcarter@gmail.com>
2021-08-03 17:45:25 +0100

    added project 43 - the counterpart to 42

---
+++ pico/43-spi-master-tx/main.cc
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+//#include "hardware/adc.h"
+//#include "hardware/clocks.h"
+//#include "hardware/flash.h"
+#include "hardware/gpio.h"
+//#include "hardware/irq.h"
+//#include "hardware/pwm.h"
+#include "hardware/spi.h"
+// #include "tusb.h" // if you want to use tud_cdc_connected()
+
+#include "pi.h"
+
+
+/*
+typedef struct spi_t {
+	spi_inst_t* inst;
+	uint sck;
+	uint cs;
+} spi_t;
+
+enum spi_type_e {
+	SPI_MASTER_TX // master mode, transmit only
+}; 
+
+void pi_spi_init(spi_t* spi, uint sck, uint cs, spi_type_e SPI_MASTER_TX)
+{
+	spi->inst = inst;
+	spi->scl = sck;
+	gpio_set_function(sck,  GPIO_FUNC_SPI); // sck
+	spi->cs = cs;
+
+	switch(SPI_MASTER_TX) {
+		case SPI_MASTER_TX: 
+			gpio_set_function(sck+1, GPIO_FUNC_SPI); // mosi
+			//gpio_set_function(sck+2, GPIO_FUNC_SPI); // miso, not requires
+			gpio_put(cs, 1);
+			pi_gpio_init(cs, OUTPUT);
+			break;
+	}
+
+
+}
+*/
+
+#define SLAVE_SCK 	2
+#define SLAVE_MOSI	4
+#define SLAVE_CS	5
+
+#define LED		25
+
+int main() 
+{
+	stdio_init_all();
+	pi_gpio_init(LED, OUTPUT);
+	pi_max7219_init();
+	pi_max7219_show_count(0);
+
+	// set up slave
+	uint baud = 2'000'000;
+	spi_init(spi0, baud);
+	spi_set_slave(spi0, true);
+	gpio_set_function(SLAVE_SCK,  GPIO_FUNC_SPI);
+	gpio_set_function(SLAVE_MOSI, GPIO_FUNC_SPI);
+	gpio_set_function(SLAVE_CS, GPIO_FUNC_SPI);
+
+	uint8_t recd[4];
+	while(1) {
+		spi_read_blocking(spi0, 0x00, recd, 4);
+		pi_gpio_toggle(LED);
+		uint32_t count = *(uint32_t*) recd;
+		pi_max7219_show_count(count++);
+	}
+
+}
+
