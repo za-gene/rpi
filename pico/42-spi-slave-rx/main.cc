193db41e2b0758ad4aee8c86eae9455576275df4
Mark Carter <alt.mcarter@gmail.com>
2021-08-03 21:31:17 +0100

    now have a working spi

--- pico/42-spi-slave-rx/main.cc
+++ pico/42-spi-slave-rx/main.cc
@@ -14,31 +14,31 @@
 
 
 /*
-typedef struct spi_t {
-	spi_inst_t* inst;
-	uint sck;
-	uint cs;
-} spi_t;
+   typedef struct spi_t {
+   spi_inst_t* inst;
+   uint sck;
+   uint cs;
+   } spi_t;
 
-enum spi_type_e {
-	SPI_MASTER_TX // master mode, transmit only
-}; 
+   enum spi_type_e {
+   SPI_MASTER_TX // master mode, transmit only
+   }; 
 
-void pi_spi_init(spi_t* spi, uint sck, uint cs, spi_type_e SPI_MASTER_TX)
-{
-	spi->inst = inst;
-	spi->scl = sck;
-	gpio_set_function(sck,  GPIO_FUNC_SPI); // sck
-	spi->cs = cs;
+   void pi_spi_init(spi_t* spi, uint sck, uint cs, spi_type_e SPI_MASTER_TX)
+   {
+   spi->inst = inst;
+   spi->scl = sck;
+   gpio_set_function(sck,  GPIO_FUNC_SPI); // sck
+   spi->cs = cs;
 
-	switch(SPI_MASTER_TX) {
-		case SPI_MASTER_TX: 
-			gpio_set_function(sck+1, GPIO_FUNC_SPI); // mosi
-			//gpio_set_function(sck+2, GPIO_FUNC_SPI); // miso, not requires
-			gpio_put(cs, 1);
-			pi_gpio_init(cs, OUTPUT);
-			break;
-	}
+   switch(SPI_MASTER_TX) {
+   case SPI_MASTER_TX: 
+   gpio_set_function(sck+1, GPIO_FUNC_SPI); // mosi
+//gpio_set_function(sck+2, GPIO_FUNC_SPI); // miso, not requires
+gpio_put(cs, 1);
+pi_gpio_init(cs, OUTPUT);
+break;
+}
 
 
 }
@@ -50,29 +50,37 @@ void pi_spi_init(spi_t* spi, uint sck, uint cs, spi_type_e SPI_MASTER_TX)
 
 #define LED		25
 
+volatile uint32_t count = 0;
+
+
+void rising(uint gpio, uint32_t events)
+{
+	if(gpio == SLAVE_CS) {
+		pi_gpio_toggle(LED);
+		pi_max7219_show_count(count);
+		printf("count = %d\n", count);
+	}
+
+	if(gpio == SLAVE_SCK) {
+		count <<= 1;
+	       	if(gpio_get(SLAVE_MOSI)) count |= 1;
+	}
+}
+
 int main() 
 {
 	stdio_init_all();
 	pi_gpio_init(LED, OUTPUT);
 	pi_max7219_init();
-	pi_max7219_show_count(666);
+	pi_max7219_show_count(0);
+	printf("Running spi-slave-rx\n");
 
-	// set up slave
-	uint baud = 100'000;
-	spi_init(spi0, baud);
-	spi_set_slave(spi0, true);
-	gpio_set_function(SLAVE_SCK,  GPIO_FUNC_SPI);
-	gpio_set_function(SLAVE_MOSI, GPIO_FUNC_SPI);
-	gpio_set_function(SLAVE_CS, GPIO_FUNC_SPI);
+	pi_gpio_init(SLAVE_SCK, INPUT);
+	gpio_set_irq_enabled_with_callback(SLAVE_SCK, GPIO_IRQ_EDGE_RISE, true, &rising);
+	pi_gpio_init(SLAVE_MOSI, INPUT);
+	pi_gpio_init(SLAVE_CS, INPUT);
+	gpio_set_irq_enabled_with_callback(SLAVE_CS, GPIO_IRQ_EDGE_RISE, true, &rising); // per SDK: gpio is ignored
 
-	uint32_t count;
-	while(1) {
-		spi_read_blocking(spi0, 0x00, (unsigned char*) &count, 4);
-		pi_gpio_toggle(LED);
-		count = __builtin_bswap32(count); // big endian to little endian
-		//count = 666;
-		pi_max7219_show_count(count);
-	}
-
+	while(1);
 }
 
