1da47c3f1d179c602d3b26e971123db18334bb92
Mark Carter <alt.mcarter@gmail.com>
2021-08-09 14:02:38 +0100

    working version of mcp4921 spi dma

--- pico/mcp4921-sawtooth-dma/README.md
+++ pico/mcp4921-sawtooth-dma/README.md
@@ -1,16 +1,6 @@
-# Play sawtooth wave using a DAC (mcp4921)
-
-Output seems pretty hopeless when attaching to a speaker,
-but it actually sounds mostly right when going through proper
-audio speakers.
-
-
+# Play sawtooth wave using a DAC (mcp4921) and DMA
 
 ## Status
 
-2021-08-09	Converted to sawtooth rather than hard-coded song
-
-2021-06-14 	Used timer interrupts
-
-2021-04-04 	Started. Hmmm, some vague nonsense is being spat out
+2021-08-09	Started. Working.
 
--- pico/mcp4921-sawtooth-dma/main.cc
+++ pico/mcp4921-sawtooth-dma/main.cc
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
+#include "hardware/dma.h"
 #include "hardware/spi.h"
 #include "hardware/gpio.h"
 #include <math.h>
 
 #include "pi.h"
 
-
-using u8 = uint8_t;
-using u16 = uint16_t;
-
-
-#define LED 25
-
+#define TOGGLE 16
 
 double framerate = 44100, saw_freq = 500;
 uint64_t period = 1'000'000/framerate;
 
+int chan;
 
+volatile uint16_t vol1;
 void mcp4921_init(void)
 {
 #define	PIN_SCK		2
@@ -28,17 +25,42 @@ void mcp4921_init(void)
 	int spi_speed = 18'000'000;
 	spi_init(spi0, spi_speed);
 	spi_set_format(spi0, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
-	gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+	//gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
 	gpio_set_function(PIN_CS,   GPIO_FUNC_SPI);
+	//gpio_init(PIN_CS);
 	gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
 	gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+
+	chan = dma_claim_unused_channel(true);
+	dma_channel_config cfg = dma_channel_get_default_config(chan);
+	channel_config_set_transfer_data_size(&cfg, DMA_SIZE_16);
+	channel_config_set_read_increment(&cfg, false);
+	channel_config_set_write_increment(&cfg, false);
+	static uint32_t dummy = 0;
+	dma_channel_configure(
+			chan,          // Channel to be configured
+			&cfg,            // The configuration we just created
+			&spi_get_hw(spi0)->dr,           // The initial write address
+			&vol1,           // The initial read address
+			1, // Number of transfers; in this case each is 1 byte.
+			false           // Start immediately?
+	);
+	dma_channel_start(chan);
+
 }
 
 void mcp4921_put(uint16_t vol)
 {
 	if(vol>4095) vol = 4095;
 	vol |= (0b11 <<12);
-	spi_write16_blocking(spi0, &vol, 1);
+	vol1 = vol;
+#if 1
+	dma_channel_transfer_from_buffer_now(chan, &vol1, 1); // assume tfr happens fast enough
+	//dma_channel_wait_for_finish_blocking(chan);
+#else
+	spi_write16_blocking(spi0, (const uint16_t*) &vol1, 1);
+#endif
+
 }
 
 volatile float y = 0, dy;
@@ -49,13 +71,13 @@ static void alarm_0_irq()
 	mcp4921_put(y);
 	y += dy;
 	if(y>4095) y=0;
-	pi_gpio_toggle(LED);
+	pi_gpio_toggle(TOGGLE);
 }
 
 int main() 
 {
 	stdio_init_all();
-	pi_gpio_init(LED, OUTPUT);
+	pi_gpio_init(TOGGLE, OUTPUT);
 	dy = 4095 * saw_freq / framerate;
 	mcp4921_init();
 	pi_alarm_init(0, alarm_0_irq, period);
