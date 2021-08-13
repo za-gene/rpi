a0199ea1e327af9ee23487dcbc996361bcca50a5
Mark Carter <alt.mcarter@gmail.com>
2021-08-13 21:54:06 +0100

    tweaks to stdio in tinyprintf

--- crunky/11-tinyprintf/kernel.c
+++ crunky/11-tinyprintf/kernel.c
@@ -2,7 +2,7 @@
 */
 
 #include <basal.h>
-#include <mini_uart.h>
+//#include <mini_uart.h>
 #include <stdio.h>
 //#include <stdarg.h>
 //#include <yastdio.h>
@@ -11,7 +11,7 @@
 
 void kernel_main()
 {
-	mini_uart_init_as_stdio();
+	//mini_uart_init_as_stdio();
 	//set_putchar(uart0_putchar);
 	puts("\nCalculate 203.56/10.5 (=19.3867), then hang");
 	volatile u32 d = 203.56 / 10.5;
