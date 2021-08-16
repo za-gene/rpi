411d3265e75fb67980e3c0b078323cc666fa1ef2
Mark Carter <alt.mcarter@gmail.com>
2021-08-16 09:04:00 +0100

    Removed proportional font from framebuffer
    Let's keep things simple, shall we?

--- crunky/lfb.c
+++ crunky/lfb.c
@@ -28,10 +28,8 @@
 #include <string.h>
 #include <basal.h>
 #include <lfb.h>
-
-//#include "uart.h"
 #include "mbox.h"
-//#include "homer.h"
+
 
 /* PC Screen Font as used by Linux Console */
 typedef struct {
@@ -47,24 +45,6 @@ typedef struct {
 } __attribute__((packed)) psf_t;
 extern volatile unsigned char _binary_font_psf_start; // fixed width
 psf_t *font = 0;
-
-/* Scalable Screen Font (https://gitlab.com/bztsrc/scalable-font2) */
-typedef struct {
-	unsigned char  magic[4];
-	unsigned int   size;
-	unsigned char  type;
-	unsigned char  features;
-	unsigned char  width;
-	unsigned char  height;
-	unsigned char  baseline;
-	unsigned char  underline;
-	unsigned short fragments_offs;
-	unsigned int   characters_offs;
-	unsigned int   ligature_offs;
-	unsigned int   kerning_offs;
-	unsigned int   cmap_offs;
-} __attribute__((packed)) sfn_t;
-extern volatile unsigned char _binary_font_sfn_start;
 
 unsigned int width, height, pitch, g_isrgb;   /* dimensions and channel order */
 unsigned char *lfb;                         /* raw frame buffer address */
@@ -186,62 +166,6 @@ void lfb_print(int x, int y, char *s)
 	while(*s) fbputchar(*s++);
 }
 
-/**
- * Display a string using proportional SSFN
- */
-void lfb_proprint(int x, int y, char *s)
-{
-	// get our font
-	sfn_t *font = (sfn_t*)&_binary_font_sfn_start;
-	unsigned char *ptr, *chr, *frg;
-	unsigned int c;
-	unsigned long o, p;
-	int i, j, k, l, m, n;
-
-	while(*s) {
-		// UTF-8 to UNICODE code point
-		if((*s & 128) != 0) {
-			if(!(*s & 32)) { c = ((*s & 0x1F)<<6)|(*(s+1) & 0x3F); s += 1; } else
-				if(!(*s & 16)) { c = ((*s & 0xF)<<12)|((*(s+1) & 0x3F)<<6)|(*(s+2) & 0x3F); s += 2; } else
-					if(!(*s & 8)) { c = ((*s & 0x7)<<18)|((*(s+1) & 0x3F)<<12)|((*(s+2) & 0x3F)<<6)|(*(s+3) & 0x3F); s += 3; }
-					else c = 0;
-		} else c = *s;
-		s++;
-		// handle carrige return
-		if(c == '\r') {
-			x = 0; continue;
-		} else
-			// new line
-			if(c == '\n') {
-				x = 0; y += font->height; continue;
-			}
-		// find glyph, look up "c" in Character Table
-		for(ptr = (unsigned char*)font + font->characters_offs, chr = 0, i = 0; i < 0x110000; i++) {
-			if(ptr[0] == 0xFF) { i += 65535; ptr++; }
-			else if((ptr[0] & 0xC0) == 0xC0) { j = (((ptr[0] & 0x3F) << 8) | ptr[1]); i += j; ptr += 2; }
-			else if((ptr[0] & 0xC0) == 0x80) { j = (ptr[0] & 0x3F); i += j; ptr++; }
-			else { if((unsigned int)i == c) { chr = ptr; break; } ptr += 6 + ptr[1] * (ptr[0] & 0x40 ? 6 : 5); }
-		}
-		if(!chr) continue;
-		// uncompress and display fragments
-		ptr = chr + 6; o = (unsigned long)lfb + y * pitch + x * 4;
-		for(i = n = 0; i < chr[1]; i++, ptr += chr[0] & 0x40 ? 6 : 5) {
-			if(ptr[0] == 255 && ptr[1] == 255) continue;
-			frg = (unsigned char*)font + (chr[0] & 0x40 ? ((ptr[5] << 24) | (ptr[4] << 16) | (ptr[3] << 8) | ptr[2]) :
-					((ptr[4] << 16) | (ptr[3] << 8) | ptr[2]));
-			if((frg[0] & 0xE0) != 0x80) continue;
-			o += (int)(ptr[1] - n) * pitch; n = ptr[1];
-			k = ((frg[0] & 0x1F) + 1) << 3; j = frg[1] + 1; frg += 2;
-			for(m = 1; j; j--, n++, o += pitch)
-				for(p = o, l = 0; l < k; l++, p += 4, m <<= 1) {
-					if(m > 0x80) { frg++; m = 1; }
-					if(*frg & m) *((unsigned int*)p) = 0xFFFFFF;
-				}
-		}
-		// add advances
-		x += chr[4]+1; y += chr[5];
-	}
-}
 
 
 int fbputchar(int  c)
