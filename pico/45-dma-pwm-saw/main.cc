3eb21d1bcf74d153074edd1b7e212ca3eb4ef782
Mark Carter <alt.mcarter@gmail.com>
2021-08-23 17:23:30 +0100

    added project 45-dma-pwm-saw

---
+++ pico/45-dma-pwm-saw/main.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+//#include "hardware/adc.h"
+//#include "hardware/clocks.h"
+//#include "hardware/flash.h"
+#include "hardware/gpio.h"
+//#include "hardware/irq.h"
+//#include "hardware/pwm.h"
+//#include "hardware/spi.h"
+// #include "tusb.h" // if you want to use tud_cdc_connected()
+
+#include "pi.h"
+
+
+int main() 
+{
+	stdio_init_all();
+	// while(!tud_cdc_connected()) sleep_ms(250); // wait for usb serial 
+
+#define BTN  14 // GPIO number, not physical pin
+#define LED  25 // GPIO of built-in LED
+	gpio_init(BTN);
+	gpio_set_dir(BTN, GPIO_IN);
+	gpio_pull_up(BTN);
+	// gpio_get() gets state of pin
+
+	gpio_init(LED);
+	gpio_set_dir(LED, GPIO_OUT);
+
+	int i = 0;
+	for(;;) {
+		printf("Hello number %d\n", i++);
+		gpio_put(LED, 1);
+		sleep_ms(100);
+		gpio_put(LED, 0);
+		sleep_ms(1000);		
+	}
+
+	return 0;
+}
+
