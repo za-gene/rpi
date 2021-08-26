862ce28965b3847ab854415ef584443f922a8528
Mark Carter <alt.mcarter@gmail.com>
2021-08-26 14:40:28 +0100

    working version of 46-dma-pwm-saw-2

--- pico/46-dma-pwm-saw-2/README.md
+++ pico/46-dma-pwm-saw-2/README.md
@@ -1,9 +1,18 @@
 # Create a saw wave using DMA and PWM
 
-Lessons learned:
-* do the `channel_config_set...()` before `dma_channel_configure()`
+Like project 45, but this time using only 8 bits
+
+## Nuances
+
+if you write to a 32 bit hardware register with an 8 bit value, then thoses 8 bits are duplicated across all 4 bytes for a 32bit write. 
+([link)(https://www.raspberrypi.org/forums/viewtopic.php?f=145&t=318404&e=1&view=unread#unread))
+It means that I have to pad the 8-bit value to 16 bits, which get duplicated in the level register, but no matter.
+I therefore decided that the best approach would therefore to just do single DMA writes of 16 bits.
+So in the end it doesn't end up too different from a timer interrupt or PWM wrap interrupt.
 
 
 ## Status
 
-2021-08-24	Started. Working.
+2021-08-26	Working.
+
+2021-08-25	Started. Partially working, but it's very quiet.
--- pico/46-dma-pwm-saw-2/main.cc
+++ pico/46-dma-pwm-saw-2/main.cc
@@ -4,9 +4,8 @@
 #include "pico/stdlib.h"
 #include <assert.h>
 #include "hardware/clocks.h"
-//#include "hardware/flash.h"
 #include "hardware/gpio.h"
-//#include "hardware/irq.h"
+#include "hardware/irq.h"
 #include "hardware/pwm.h"
 #include "hardware/dma.h"
 // #include "tusb.h" // if you want to use tud_cdc_connected()
@@ -18,11 +17,22 @@
 
 #define TOP 255
 #define NSAMPS 256
-uint32_t table[NSAMPS];
 unsigned int slice_num;
 int dma_chan;
 
 #define pinfo(x) printf("%s = %u\n", #x, x);
+
+volatile uint16_t val = 0;
+
+void dma_handler()
+{
+	//dma_irqn_acknowledge_channel(??, dma_chan);
+	dma_hw->ints0=1u<<dma_chan; // clear the interrupt request
+	dma_channel_transfer_from_buffer_now(dma_chan, &val, 1);
+	val++;
+	if(val == 256) val = 0;
+
+}
 
 int main() 
 {
@@ -30,16 +40,10 @@ int main()
 	// while(!tud_cdc_connected()) sleep_ms(250); // wait for usb serial 
 	puts("\nsawtooth wave via dma and pwm");
 
-	// fill in table
 	const int pwm_chan = pwm_gpio_to_channel(SPK);
-	//const int pwm_channel_shift = pwm_chan == 0 ?  0 : 16;
-	for(int i = 0; i< NSAMPS; i++) {
-		table[i] = i << (pwm_chan ? PWM_CH0_CC_B_LSB : PWM_CH0_CC_A_LSB);
-	}
-	printf("channel is: %d\n", pwm_chan); 
+	pinfo(pwm_chan); 
 
 	// set up pwm
-	pinfo(count_of(table));
 	pinfo(PWM_CH0_CC_A_LSB);
 	pinfo(PWM_CH0_CC_B_LSB);
 	const int freq = 500 * NSAMPS; // 500Hz sawtooth
@@ -59,23 +63,33 @@ int main()
 
 	dma_chan = dma_claim_unused_channel(true);
 	dma_channel_config cfg = dma_channel_get_default_config(dma_chan);
-	channel_config_set_transfer_data_size(&cfg, DMA_SIZE_32);
+	channel_config_set_transfer_data_size(&cfg, DMA_SIZE_16);
 	channel_config_set_read_increment(&cfg, true);
 	channel_config_set_dreq(&cfg, DREQ_PWM_WRAP0 + slice_num); // write data at pwm frequency
 	//channel_config_set_write_increment(&cfg, false);
+	//uint32_t write_address = (uint32_t) &pwm_hw->slice[slice_num].cc; // base address
+	//if(pwm_chan) write_address += 2; // adjust for channel
+	//write_address +=1;
 	dma_channel_configure(
-			dma_chan,          // Channel to be configured
-			&cfg,            // The configuration we just created
-			&pwm_hw->slice[slice_num].cc,           // The initial write address
-			table,           // The initial read address
-			NSAMPS, // Number of transfers
-			false           // Start immediately?
-			);
-	//dma_channel_start(dma_chan); // didn't help
+		dma_chan,          // Channel to be configured
+		&cfg,            // The configuration we just created
+		//(volatile void*) write_address,           // The initial write address
+		&pwm_hw->slice[slice_num].cc, // write address
+		&val,           // The initial read address
+		1, // Number of transfers
+		false           // Start immediately?
+		);
+
+	dma_channel_set_irq0_enabled(dma_chan,true);
+	irq_set_exclusive_handler(DMA_IRQ_0,dma_handler);
+	irq_set_enabled(DMA_IRQ_0,true);
+	dma_handler(); // kick off
+	//dma_channel_start(dma_chan); // helps?
 
 	while(1) {
-		dma_channel_transfer_from_buffer_now(dma_chan, table, NSAMPS);
-		dma_channel_wait_for_finish_blocking(dma_chan);
+		tight_loop_contents();
+		//dma_channel_transfer_from_buffer_now(dma_chan, table, NSAMPS);
+		//dma_channel_wait_for_finish_blocking(dma_chan);
 		//dma_channel_start(dma_chan);
 	}
 	return 0;
