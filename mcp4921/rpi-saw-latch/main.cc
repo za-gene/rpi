e0d0d3682bf80ebff78a6bda7a17f2f50a2cad02
Mark Carter <alt.mcarter@gmail.com>
2021-09-08 20:10:47 +0100

    working version of latching

--- mcp4921/rpi-saw-latch/main.cc
+++ mcp4921/rpi-saw-latch/main.cc
@@ -15,19 +15,19 @@ typedef uint16_t u16;
 
 mcp4921 mcp;
 
-#define SCALE	16
-#define SAMPLES (4096/SCALE)
-//u16 sines[SAMPLES];
+//#define SCALE	16
+//#define SAMPLES (4096/SCALE)
 
-#define PWM_PIN RPI_GPIO_P1_12 // physical pin 18
+#define PWM_PIN 	RPI_GPIO_P1_12 // physical pin 18
+#define PWM_FRAMERATE 	16'000
+#define PWM_DIVIDER 	BCM2835_PWM_CLOCK_DIVIDER_2
+#define PWM_RANGE 	(1'200'000 * 16 /PWM_FRAMERATE / PWM_DIVIDER)
 
 void pwm_init(void)
 {
 #define PWM_CHANNEL 0
-#define PWM_FRAMERATE 16'000
-#define PWM_RANGE (1'200'000/PWM_FRAMERATE)
 	bcm2835_gpio_fsel(PWM_PIN, BCM2835_GPIO_FSEL_ALT5);
-	bcm2835_pwm_set_clock(BCM2835_PWM_CLOCK_DIVIDER_16); // produces a clock rate of 1.2MHz
+	bcm2835_pwm_set_clock(PWM_DIVIDER);
 	bcm2835_pwm_set_mode(PWM_CHANNEL, 1, 1);
 	bcm2835_pwm_set_range(PWM_CHANNEL, PWM_RANGE);
 	bcm2835_pwm_set_data(PWM_CHANNEL, PWM_RANGE/2);
@@ -43,7 +43,9 @@ int main()
 
 
 	// initialise
-	const double freq = 200;
+	const double freq = 400;
+	const double dy = freq / PWM_FRAMERATE ;
+	printf("dy=%f\n", dy);
 
 	/*
 	   double lo = 1.0/3.3 * hi; // 1V
@@ -56,13 +58,18 @@ int main()
 	   }
 	   */
 
-	int delay = 1'000'000 / freq / SAMPLES;
-	printf("delay=%d\n", delay);	
+	//int delay = 1'000'000 / freq / SAMPLES;
+	//printf("delay=%d\n", delay);
+	double y = 0;
 	while(1)  {
-		for (int i = 0; i < SAMPLES; i++) {
-			mcp.write(i*SCALE);
-			bcm2835_delayMicroseconds(delay);
+		if(bcm2835_gpio_eds(PWM_PIN)) { // did we detect an event (rising edge)
+			bcm2835_gpio_set_eds(PWM_PIN); // reset the event flag
+			mcp.write(y*4095);
+			y += dy;
+			if(y>=1) y = 0;
+			//if(++i >= SAMPLES) i = 0;
 		}
+		bcm2835_delayMicroseconds(1);
 	}
 
 	return 0;
