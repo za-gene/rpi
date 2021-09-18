a6983927529cb14037857fcd59c578c219aec65d
Mark Carter <alt.mcarter@gmail.com>
2021-09-18 23:31:42 +0100

    century tweaks

--- audio/rpi-century/main.c
+++ audio/rpi-century/main.c
@@ -29,25 +29,52 @@ int main()
 	// the following are defaults
 	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
 	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);
-	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536);
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536);
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_64); // 6.25 MHz on Pi3
 	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_256);
+
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_512);
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_1024);
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_2048);
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_4096); // too fast
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_32768); //works
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_16384);
+	//bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_8192); // works
+	const int mhz = 1000 * 1000;
+	bcm2835_spi_set_speed_hz(1*mhz);
 	bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
 	//bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);
-	int count = 1;
+	uint8_t tx = 1, rx;
+
+
+	// send note 440 Hz
+	uint8_t b;
+	for(int i = 0; i< 8; i++) {
+		b = 0b10010000;
+		bcm2835_spi_writenb(&b, 1 ); // note on
+		//bcm2835_delay(2); 
+		b = 73;
+		bcm2835_spi_writenb(&b, 1 ); // note on
+		bcm2835_delay(500); 
+		b = 0b10000000;
+		bcm2835_spi_writenb(&b, 1 ); // note on
+		bcm2835_delay(500); 
+	}
+	return 0;
 
 
 
 	while(1) {
-		//usleep(3000);
 		//printf("Sending %d\n", count);
-		uint8_t rx = bcm2835_spi_transfer(count);
-		printf("Received %d\n", rx);
-		//if((rx&1)==0) continue;
-		count++;
-		if(count == 100) count = 1;
-		//printf("Read number %d from SPI: %d\n", count++, data);
-		sleep(1);
-
+		while(1) {
+			//usleep(1);
+			bcm2835_delayMicroseconds(10);
+			rx = bcm2835_spi_transfer(tx);
+			//break;
+			if(rx != 0) break;
+		}
+		tx++;
+		if(tx==100) tx = 1;
 	}
 
 	return 0;
